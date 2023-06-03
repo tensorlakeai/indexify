276791a2c56523582f9530d245bac6a35051291f
diptanu <diptanuc@gmail.com>
2023-06-03 20:37:58 +0000

    shutdown handler

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use clap::{Parser, Subcommand};
 use std::sync::Arc;
 use tracing::log::info;
 
-/// The command-line interface (CLI) for the Indexify Server.
-/// The CLI provides commands for starting the server and initializing the configuration file.
 #[derive(Debug, Parser)]
 #[command(name = "indexify")]
 #[command(about = "CLI for the Indexify Server", long_about = None)]
@@ -13,28 +11,20 @@ struct Cli {
     command: Commands,
 }
 
-/// An enumeration of available subcommands for the Indexify Server CLI.
 #[derive(Debug, Subcommand)]
 enum Commands {
-    /// The `start` subcommand, which starts the Indexify Server.
-    /// The server reads its configuration from the specified configuration file.
     #[command(about = "Start the server")]
     Start {
-        /// The path to the configuration file that the server should use.
         #[arg(short, long)]
         config_path: String,
     },
-    /// The `init-config` subcommand, which initializes a new configuration file.
-    /// The generated configuration file can be customized and used to start the server.
     InitConfig {
-        /// The path where the new configuration file should be created.
         config_path: String,
     },
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // Initialize the tracing subscriber for logging.
     let subscriber = tracing_subscriber::FmtSubscriber::new();
     tracing::subscriber::set_global_default(subscriber)?;
 
