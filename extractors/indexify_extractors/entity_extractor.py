85ca7355d653e164d49eb359328d571b14bc4b0d
diptanu <diptanuc@gmail.com>
2023-08-08 08:29:42 +0000

    added support for input_params

--- extractors/indexify_extractors/entity_extractor.py
+++ extractors/indexify_extractors/entity_extractor.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from typing import List, Literal
-from transformers import AutoTokenizer, AutoModelForTokenClassification
-from transformers import pipeline
+from span_marker import SpanMarkerModel
 from decimal import Decimal 
 from enum import Enum
 import json
@@ -21,33 +20,39 @@ class ExtractedAttributes:
 class EntityType(Enum): 
     def get_entity_type(type: str) -> str:
         entity_type_map = {
-            "B-PER": "Person",
-            "B-ORG": "Organization",
-            "I-ORG": "Organization",
-            "B-LOC": "Location",
-            "I-LOC": "Location"
+            "PER": "Person",
+            "ORG": "Organization",
+            "LOC": "Location",
+            "ANIM": "Animal",
+            "BIO": "Biological",
+            "CELL": "Celestial",
+            "DIS": "Disease",
+            "EVE": "Event",
+            "FOOD": "Food",
+            "INST": "Instrument",
+            "MEDIA": "Media",
+            "PLANT": "Plant",
+            "MYTH": "Mythological",
+            "TIME": "Time",
+            "VEHI": "Vehicle",
          }   
         return entity_type_map.get(type, "INVALID")
     
 
 class EntityExtractor:
    
-    def __init__(self, model_name: str = "dslim/bert-base-NER"):
-        self.model_name = model_name
-        # Load model - https://huggingface.co/dslim/bert-base-NER
-        self._tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        self._model = AutoModelForTokenClassification.from_pretrained(self.model_name)
-        self._ctx_pipeline = pipeline("ner", model=self._model, tokenizer=self._tokenizer)
+    def __init__(self):
+        self._model = SpanMarkerModel.from_pretrained("tomaarsen/span-marker-xlm-roberta-base-multinerd")
 
     def extract(self, content: List[Content], params: dict[str, str]) -> List[ExtractedAttributes]:
         content_texts = [c.data for c in content]
-        results = self._ctx_pipeline(content_texts)
+        results = self._model.predict(content_texts)
         attributes = []
         for (i, ner_list) in enumerate(results):
             content_id = content[i].id
             for ner in ner_list:
-                name = EntityType.get_entity_type(ner["entity"])
-                value = ner["word"]
+                name = EntityType.get_entity_type(ner["label"])
+                value = ner["span"]
                 score = ner["score"]
                 data = json.dumps({"entity": name, "value": value, "score": str(score)})
                 attributes.append(ExtractedAttributes(content_id=content_id, json=data))
@@ -59,9 +64,8 @@ class EntityExtractor:
         return ExtractorInfo(
             name="EntityExtractor",
             description="EntityExtractor",
-            input_params="{'type': 'object'}",
             output_datatype="attributes",
-            input_params=EntityExtractionInputParams.model_json_schema(),
+            input_params=json.dumps(EntityExtractionInputParams.model_json_schema()),
             output_schema= schema_json,
         )
 
