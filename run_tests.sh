3260da54bae6fed0d10e771d1c0bc6a56ccaa13c
diptanu <diptanuc@gmail.com>
2023-10-14 18:43:45 +0000

    Refactor run scripts

--- run_tests.sh
+++ run_tests.sh
@@ -5,34 +5,21 @@ set -ex
 function stop_docker()
 {
   echo "stopping qdrant_test"
-  docker stop qdrant_test
-  docker stop postgres_test
+  docker stop indexify-local-qdrant
+  docker stop indexify-local-postgres
 }
 
-# Ensure current path is project root
-#cd "$(dirname "$0")/../"
-
-QDRANT_VERSION='v1.4.1'
 QDRANT_HOST='localhost:6333'
-
-docker run -d --rm \
-           --network=host \
-           --name qdrant_test qdrant/qdrant:${QDRANT_VERSION}
-
-docker run --rm -p 5432:5432 \
-          --name=postgres_test -e POSTGRES_PASSWORD=postgres \
-          -e POSTGRES_DB=indexify_test -d ankane/pgvector
 
 trap stop_docker SIGINT
 trap stop_docker ERR
+
+make local-dev
 
 until curl --output /dev/null --silent --get --fail http://$QDRANT_HOST/collections; do
   printf 'waiting for server to start...'
   sleep 5
 done
-
-timeout 90s bash -c "until docker exec postgres_test pg_isready ; do sleep 5 ; done"
-docker exec postgres_test psql -U postgres -c 'create database indexify;'
 
 pip install .
 
