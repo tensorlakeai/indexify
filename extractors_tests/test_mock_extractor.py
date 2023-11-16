feb6433a52d0bef203fc571d139ff61243ca6b90
Diptanu Choudhury <diptanuc@gmail.com>
2023-11-16 09:25:30 -0800

    fixed mock extractor test

--- extractors_tests/test_mock_extractor.py
+++ extractors_tests/test_mock_extractor.py
@@ -1,6 +1,6 @@
 import unittest
 
-from indexify_extractor_sdk.base_extractor import ExtractorWrapper, Content, ContentType
+from indexify_extractor_sdk.base_extractor import ExtractorWrapper, Content
 
 from indexify_extractor_sdk.mock_extractor import MockExtractor, InputParams
 
@@ -11,14 +11,14 @@ class TestMockExtractor(unittest.TestCase):
     def test_mock_extractor(self):
         params = InputParams(a=1, b="foo")
         e = MockExtractor()
-        extracted_content = e.extract([Content(content_type=ContentType.Text, data="Hello World")], params)
+        extracted_content = e.extract([Content(content_type="text", data="Hello World")], params)
         self.assertEqual(len(extracted_content), 1)
         self.assertEqual(len(extracted_content[0]), 3)
 
 
     def test_extractor_wrapper(self):
         e = ExtractorWrapper("indexify_extractor_sdk.mock_extractor:MockExtractor")
-        extracted_content = e.extract([Content(content_type=ContentType.Text, data="Hello World")], '{"a": 1, "b": "foo"}')
+        extracted_content = e.extract([Content(content_type="text", data="Hello World")], '{"a": 1, "b": "foo"}')
         self.assertEqual(len(extracted_content), 1)
         self.assertEqual(len(extracted_content[0]), 3)
 
