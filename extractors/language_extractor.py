76987e7b6d4a082ad84dd58eb27d862a437c4658
David <david.yenicelik@gmail.com>
2023-11-20 14:33:55 +0000

    language extractor, will proceed with pdf->markdown

--- extractors/language_extractor.py
+++ extractors/language_extractor.py
@@ -1,51 +1,61 @@
 from typing import List, Literal
-from decimal import Decimal
-import json
-from indexify_extractor_sdk.base_extractor import ExtractorInfo, Content, Attributes, Extractor
-from lingua import LanguageDetectorBuilder
 from pydantic import BaseModel
+from indexify_extractor_sdk import (
+    Extractor,
+    Feature,
+    ExtractorSchema,
+    EmbeddingSchema,
+    Content,
+)
+import json
+from indexify_extractor_sdk.base_embedding import (
+    EmbeddingInputParams,
+)
+from indexify_extractor_sdk.sentence_transformer import SentenceTransformersEmbedding
+from lingua import LanguageDetectorBuilder
+
 
 class LanguageExtractionInputParams(BaseModel):
     overlap: int = 0
     text_splitter: Literal["char", "token", "recursive", "new_line"] = "new_line"
 
-class LanguageExtractionOutputSchema(BaseModel):
-    entity: str
-    value: str
-    score: Decimal
-
 class LanguageExtractor(Extractor):
     def __init__(self):
-        self._model = LanguageDetectorBuilder.from_all_languages().with_low_accuracy_mode().build()
+        super().__init__()
+        self._model = LanguageDetectorBuilder.from_all_languages().build()
 
     def extract(
-        self, content: List[Content], params: InputParams
+        self, content: List[Content], params: LanguageExtractionInputParams
     ) -> List[List[Content]]:
-        content_texts = [c.data for c in content]
-        content_texts = [c.data for c in content]
-        attributes = []
+        content_texts = [c.data.decode("utf-8") for c in content]
+        out = []
         for i, x in enumerate(content):
             language = self._model.detect_language_of(content_texts[i])
             confidence = self._model.compute_language_confidence(content_texts[i], language)
-            data = json.dumps({"entity": "language", "value": language.name, "score": str(confidence)})
-            attributes.append(
-                    Attributes(
-                        content_id=x.id, text=content_texts[i], attributes=data
-                    )
-                )
-        return attributes
+            # TODO: Could be modified depending on the database we have
+            data = {"language": language.name, "score": str(confidence)}
+            out.append(
+                [Content.from_text(
+                    text=content_texts[i],
+                    feature=Feature.metadata(value=data, name="language"),
+                )]
+            )
+        print("Out is: ", out)
+        return out
 
     def extract_query_embeddings(self, query: str) -> List[float]:
-        raise NotImplementedError
+        # TODO: This will not be implemented for this extractor
+        raise NotImplementedError 
 
     def schemas(self) -> ExtractorSchema:
-        raise NotImplementedError
-
-    def info(self) -> ExtractorInfo:
+        """
+        Returns a list of options for indexing.
+        """
         input_params = LanguageExtractionInputParams()
-        return ExtractorInfo(
-            name="LanguageExtractor",
-            description="LanguageExtractor",
-            input_params=input_params,
-            output_schema=LanguageExtractionOutputSchema,
+        # TODO If it's metadata, how do we extract things
+        # This extractor does not return any embedding, only a dictionary!
+        return ExtractorSchema(
+            embedding_schemas={},
+            input_params=json.dumps(input_params.model_json_schema()),
         )
+
