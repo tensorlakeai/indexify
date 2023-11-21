38bc2e2fe110034cedfe93fcfd90fe17096ad1e0
David <david.yenicelik@gmail.com>
2023-11-21 17:57:26 +0000

    extractor is working, will document the process and start to improve it

--- extractors/simple_invoice_parser.py
+++ extractors/simple_invoice_parser.py
@@ -1,8 +1,22 @@
-import sentence_transformer
-import 
+import pytesseract
+import sentencepiece
 from transformers import pipeline
 from pdf2image import convert_from_bytes
 from transformers import DonutProcessor, VisionEncoderDecoderModel
+
+import re
+from PIL import Image
+
+import torch
+
+import requests
+from io import BytesIO
+
+import locale
+from pdf2image import convert_from_path
+
+import timeit
+import json
 
 from typing import List, Literal
 from pydantic import BaseModel
@@ -13,12 +27,10 @@ from indexify_extractor_sdk import (
     EmbeddingSchema,
     Content,
 )
-import json
 from indexify_extractor_sdk.base_embedding import (
     EmbeddingInputParams,
 )
 from indexify_extractor_sdk.sentence_transformer import SentenceTransformersEmbedding
-from lingua import LanguageDetectorBuilder
 
 
 class SimpleInvoiceParserInputParams(BaseModel):
@@ -31,52 +43,60 @@ class SimpleInvoiceParserExtractor(Extractor):
         self.processor = DonutProcessor.from_pretrained("to-be/donut-base-finetuned-invoices")
         self.model = VisionEncoderDecoderModel.from_pretrained("to-be/donut-base-finetuned-invoices")
         # TODO: Is this for example how we would pick it up? Probably the model would still need to be defined by the user i.e. how it should be used
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model.to(device)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model.to(self.device)
 
-    def _query(image):
+    def _process_document(self, image):
         # prepare encoder inputs
-        pixel_values = processor(image, return_tensors="pt").pixel_values
+        pixel_values = self.processor(image, return_tensors="pt").pixel_values
 
         # prepare decoder inputs
         task_prompt = "<s_cord-v2>"
-        decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+        decoder_input_ids = self.processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
 
         # generate answer
-        outputs = model.generate(
-            pixel_values.to(device),
-            decoder_input_ids=decoder_input_ids.to(device),
-            max_length=model.decoder.config.max_position_embeddings,
+        outputs = self.model.generate(
+            pixel_values.to(self.device),
+            decoder_input_ids=decoder_input_ids.to(self.device),
+            max_length=self.model.decoder.config.max_position_embeddings,
             early_stopping=True,
-            pad_token_id=processor.tokenizer.pad_token_id,
-            eos_token_id=processor.tokenizer.eos_token_id,
+            pad_token_id=self.processor.tokenizer.pad_token_id,
+            eos_token_id=self.processor.tokenizer.eos_token_id,
             use_cache=True,
             num_beams=1,
-            bad_words_ids=[[processor.tokenizer.unk_token_id]],
+            bad_words_ids=[[self.processor.tokenizer.unk_token_id]],
             return_dict_in_generate=True,
         )
 
         # postprocess
-        sequence = processor.batch_decode(outputs.sequences)[0]
-        sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+        sequence = self.processor.batch_decode(outputs.sequences)[0]
+        sequence = sequence.replace(self.processor.tokenizer.eos_token, "").replace(self.processor.tokenizer.pad_token, "")
         sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
         # img2.update(visible=False)
-        return processor.token2json(sequence), image
+        return self.processor.token2json(sequence), image
 
     def extract(
-        self, content: List[Content], params: LanguageExtractionInputParams
+        self, content: List[Content], params: SimpleInvoiceParserInputParams
     ) -> List[List[Content]]:
-        content_texts = [c.data.decode("utf-8") for c in content]
+        # TODO: Assert that filepaths are provided
+        content_filebytes = [c.data for c in content]
+
+        # This assumes that filepaths are local, we should prob use requests if it is on AWS or similar
+        # response = requests.get("https://huggingface.co/spaces/impira/docquery/resolve/2359223c1837a7587402bda0f2643382a6eefeab/invoice.png")
+        # img = Image.open(BytesIO(response.content)).convert("RGB")
+        print("Filebytes are: ", content_filebytes)
+        # TODO: Right now it only looks at the first image! We should probably flatten it and do it for each page!
+        images = [convert_from_bytes(x)[0].convert("RGB") for x in content_filebytes]
+        print("Images are: ", images)
+
         out = []
         for i, x in enumerate(content):
-            language = self._model.detect_language_of(content_texts[i])
-            confidence = self._model.compute_language_confidence(content_texts[i], language)
-            # TODO: Could be modified depending on the database we have
-            data = {"language": language.name, "score": str(confidence)}
+            print("i, x are: ", i, x)
+            data = self._process_document(images[i])[0]  # Key 1 includes the image, which we ignore in this case
             out.append(
                 [Content.from_text(
-                    text=content_texts[i],
-                    feature=Feature.metadata(value=data, name="language"),
+                    text="",  # TODO: Diptanu, what do we do for PDFs? Do you want to save the raw bytes too, I feel like this is unnecessary? Also, I felt like these would be stored in a database _before_ processing, not after
+                    feature=Feature.metadata(value=data, name="invoice_simple_donut"),
                 )]
             )
         return out
@@ -89,7 +109,7 @@ class SimpleInvoiceParserExtractor(Extractor):
         """
         Returns a list of options for indexing.
         """
-        input_params = LanguageExtractionInputParams()
+        input_params = SimpleInvoiceParserExtractor()
         # TODO If it's metadata, how do we extract things
         # This extractor does not return any embedding, only a dictionary!
         return ExtractorSchema(
