e76970cfd1593d2b4a878ca11812282b640080d1
David <david.yenicelik@gmail.com>
2023-11-22 11:12:06 +0000

    cleaned up identity hash embedding

--- extractors/identity_hash_embedding.py
+++ extractors/identity_hash_embedding.py
@@ -1,30 +1,22 @@
-from typing import List
-from indexify_extractor_sdk import (
-    ExtractorSchema,
-    EmbeddingSchema,
-    Content,
-)
-from indexify_extractor_sdk.base_embedding import (
-    BaseEmbeddingExtractor,
-    EmbeddingInputParams,
-)
-
 import hashlib
 import numpy as np
+from typing import List
 
-"""
-    Implements a Hash Extractor, which can be used to find duplicates within the dataset.
-    It hashes the text into bytes, and interprets these are a numpy array.
+from indexify_extractor_sdk import (ExtractorSchema, EmbeddingSchema)
+from indexify_extractor_sdk.base_embedding import (BaseEmbeddingExtractor, EmbeddingInputParams)
 
-    We can extend this by LocalitySensitiveHashing, to also account for small perturbations in the input bytes.
-
-    This is equivalent to an identity mapping (with the sample-size n large enough, there will be collisions, but this is highly unlikely )
-"""
 class IdentityHashEmbedding(BaseEmbeddingExtractor):
+    """
+        Implements a Hash Extractor, which can be used to find duplicates within the dataset.
+        It hashes the text into bytes, and interprets these are a numpy array.
+
+        We can extend this by LocalitySensitiveHashing, to also account for small perturbations in the input bytes.
+
+        This is equivalent to an identity mapping (with the sample-size n large enough, there will be collisions, but this is highly unlikely )
+    """
 
     def __init__(self):
         super(IdentityHashEmbedding, self).__init__(max_context_length=128)
-        self._model = hashlib.sha256()
 
     def extract_embeddings(self, texts: List[str]) -> List[List[float]]:
         return [self._embed(text) for text in texts]
@@ -42,6 +34,7 @@ class IdentityHashEmbedding(BaseEmbeddingExtractor):
         )
 
     def _embed(self, text) -> List[float]:
-        self._model.update(bytes(text, 'utf-8'))
-        out = self._model.digest()
+        model = hashlib.sha256()
+        model.update(bytes(text, 'utf-8'))
+        out = model.digest()
         return np.frombuffer(out, dtype=np.int8).tolist()
