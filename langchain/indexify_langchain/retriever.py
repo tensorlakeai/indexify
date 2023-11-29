1f4f968428d380e1492289b85f55d43ad7d88450
Lucas Jackson <lucasjackson5815@gmail.com>
2023-11-29 11:45:01 -0800

    remove unused imports

--- langchain/indexify_langchain/retriever.py
+++ langchain/indexify_langchain/retriever.py
@@ -3,7 +3,6 @@ from typing import Any, List, Optional
 from langchain_core.documents import Document
 from langchain_core.pydantic_v1 import root_validator
 from langchain_core.retrievers import BaseRetriever
-from indexify import Repository, FilterBuilder
 
 from langchain.callbacks.manager import CallbackManagerForRetrieverRun
 
