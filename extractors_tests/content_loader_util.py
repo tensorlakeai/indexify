854609bcffc3492a3718cdba8c3073daf468b432
Lucas Jackson <lucasjackson5815@gmail.com>
2023-12-01 15:00:33 -0800

    updated extractor_tests name / remove extractors

--- extractors_tests/content_loader_util.py
+++
@@ -1,7 +0,0 @@
-from indexify_extractor_sdk import Content
-from io import BytesIO
-
-def load_pdf_as_content(file: str) -> Content:
-    with open("extractors_tests/data/test.pdf", "rb") as fh:
-        b = fh.read()
-    return Content(id=file, content_type="pdf", data=b)
