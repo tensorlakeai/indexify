d01f6ae89da8432fab1893096edfb8e0decc2161
Rakshith Ravi <rakshith.ravi@gmx.com>
2024-01-16 07:28:25 +0000

    Added a publishing workflow for GHA (#225)
    * Added a publishing workflow for GHA
    
    * Fixed upload artifact syntax
    
    * Fixed nightly toolchain and added output for version number
    
    * Added version for extraction of output
    
    * Added nightly fmt.
    
    * Added toolchain and component. Why is rustup so weird?
    
    * Formatted the code. At least nightly rustfmt works
    
    * Added protobuf compilers for the code to build
    
    * Added support for m1 macs and protobufs
    
    * Changed secret name to RELEASE_TOKEN
    
    * Added downloading artifacts for creating release
    
    * Debugging version mismatch
    Added explicit files for release assets
    
    * Added output for extraction step
    
    * Write to github output for package version
    
    * Add debug message for release directory
    
    * Test upload action
    
    * Fixed extraction step name. Fml
    
    * Reverted testing stuff
    
    * Added explicit file names
    
    * Use in-tree tmp folder instead of global
    
    * Removed tmp folder
    
    * Debugging GHA release binaries not being uploaded
    
    * Moved to archived GHA release type. Softprops thing doesn't seem to work
    
    * Fixed typo
    
    * Removed absolute paths for relative paths
    
    * Added write permissions
    
    * Added unzipping of GHA artifacts
    
    * Fixed typo for download URL
    
    * Updated tmp folder
    
    * Changed from zip file to directory

--- src/coordinator_client.rs
+++ src/coordinator_client.rs
@@ -28,7 +28,13 @@ impl CoordinatorClient {
 
         let client = CoordinatorServiceClient::connect(format!("http://{}", &self.addr))
             .await
-            .map_err(|e| anyhow!("unable to connect to coordinator: {} at addr {}", e, self.addr))?;
+            .map_err(|e| {
+                anyhow!(
+                    "unable to connect to coordinator: {} at addr {}",
+                    e,
+                    self.addr
+                )
+            })?;
         clients.insert(self.addr.to_string(), client.clone());
         Ok(client)
     }
