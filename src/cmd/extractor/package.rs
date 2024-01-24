c01c55456d2a732ec3a5643131d15de56180ec1b
diptanu <diptanuc@gmail.com>
2024-01-24 00:15:14 +0000

    Removed verbose from the package command

--- src/cmd/extractor/package.rs
+++ src/cmd/extractor/package.rs
@@ -4,16 +4,15 @@ use crate::{cmd::GlobalArgs, prelude::*};
 
 #[derive(Debug, ClapArgs)]
 pub struct Args {
-    #[arg(short, long)]
-    dev: bool,
-
     #[arg(long)]
-    verbose: bool,
+    dev: bool,
 }
 
 impl Args {
-    pub async fn run(self, extractor_config_path: String, _: GlobalArgs) {
-        let Self { dev, verbose } = self;
+    pub async fn run(self, extractor_config_path: String, global_args: GlobalArgs) {
+        let Self { dev } = self;
+
+        let verbose = global_args.verbosity > 1;
 
         info!("starting indexify packager, version: {}", crate::VERSION);
         crate::package::Packager::new(extractor_config_path, dev)
