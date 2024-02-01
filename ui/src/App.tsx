52202b95005f145d88c87f2236ed36dac00b9ea0
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-01 11:25:33 -0800

    wip

---
+++ ui/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return <Dashboard />;
+}
+
+export default App;
