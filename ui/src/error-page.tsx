b6d9cf080dd3fa1125cc9b1a489d38806407f41e
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-01 18:20:12 -0800

    wip error page

---
+++ ui/src/error-page.tsx
@@ -0,0 +1,31 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+export function ErrorPage() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div id="error-page">
+        <h1>Oops! {error.status}</h1>
+        <p>{error.statusText}</p>
+        {error.data?.message && (
+          <p>
+            <i>{error.data.message}</i>
+          </p>
+        )}
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div id="error-page">
+        <h1>Oops! Unexpected Error</h1>
+        <p>Something went wrong.</p>
+        <p>
+          <i>{error.message}</i>
+        </p>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+}
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Dashboard from "./routes/Dashboard";
+import Dashboard from "./routes/root";
+import { ErrorPage } from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/*",
     element: <Dashboard />,
-    // loader: rootLoader,
+    errorElement: <ErrorPage />,
     // children: [
     //   {
     //     path: "test",
