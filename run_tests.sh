0c88c6359209ed005d38692223bf9e3b30bcc2ae
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-05 16:46:25 -0800

    undo remove

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,7 @@ set -ex
 function stop_docker()
 {
   echo "stopping qdrant_test"
+  docker stop indexify-local-qdrant
   docker stop indexify-local-postgres
   docker stop indexify-local-opensearch
 }
@@ -15,6 +16,11 @@ trap stop_docker SIGINT
 trap stop_docker ERR
 
 make local-dev
+
+until curl --output /dev/null --silent --get --fail http://$QDRANT_HOST/collections; do
+  printf 'waiting for server to start...'
+  sleep 5
+done
 
 pip install .[test]
 
