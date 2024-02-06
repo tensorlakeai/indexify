ddba793f6340a3d984f1bf0e6a1b2df08f0efc3b
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-05 17:50:14 -0800

    Fix sample_config.yaml test (#309)
    * update tests
    
    * undo remove
    
    * format

--- src/server_config.rs
+++ src/server_config.rs
@@ -446,11 +446,11 @@ mod tests {
         let config = super::ServerConfig::from_path("sample_config.yaml").unwrap();
         assert_eq!(
             config.index_config.index_store,
-            super::IndexStoreKind::Qdrant
+            super::IndexStoreKind::PgVector
         );
         assert_eq!(
-            config.index_config.qdrant_config.unwrap().addr,
-            "http://qdrant:6334".to_string()
+            config.index_config.pg_vector_config.unwrap().addr,
+            "postgres://postgres:postgres@postgres/indexify".to_string()
         );
     }
 }
