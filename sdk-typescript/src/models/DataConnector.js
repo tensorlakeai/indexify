3c98097b28079ecbcb2dc5a8104145dd08ada850
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-07 19:19:19 -0800

    remove typescript (#326)

--- sdk-typescript/src/models/DataConnector.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-Object.defineProperty(exports, "__esModule", { value: true });
--- sdk-typescript/src/models/Text.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type Text = {
-    metadata: Record<string, any>;
-    text: string;
-};
-
