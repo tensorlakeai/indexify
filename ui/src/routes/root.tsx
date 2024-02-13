ef56e4075bc85b53dd8f3cfd30a81eb4e90de328
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-12 17:22:30 -0800

    fix warnings

--- ui/src/routes/root.tsx
+++ ui/src/routes/root.tsx
@@ -1,19 +1,12 @@
 import * as React from "react";
 import { styled, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import MuiDrawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import { MainListItems } from "../components/listItems";
 import {
   LoaderFunctionArgs,
   Outlet,
