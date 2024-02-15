48ee203df36033b221704be0df71415ce23571fb
Rakshith Ravi <rakshith.ravi@gmx.com>
2024-02-15 20:49:55 +0530

    Added extractors for init compose. Added network aliases instead of hard-coded IP address to compose files (#350)

--- src/cmd/init_compose.rs
+++ src/cmd/init_compose.rs
@@ -3,28 +3,43 @@ use clap::Args as ClapArgs;
 
 use super::GlobalArgs;
 
+struct TemplateExtractorValue {
+    docker_image: String,
+    service_name: String,
+}
+
 #[derive(Template)]
 #[template(path = "docker_compose.yaml.jinja2")]
 struct DockerComposeTemplate {
-    // Add fields here when we templatize this
+    extractors: Vec<TemplateExtractorValue>,
 }
 
 #[derive(Debug, ClapArgs)]
 pub struct Args {
     #[arg(short, long)]
     path: Option<String>,
+    #[arg(long)]
+    extractor: Vec<String>,
 }
 
 impl Args {
     pub async fn run(self, _: GlobalArgs) {
-        let Self { path } = self;
+        let Self { path, extractor } = self;
         let path = path.unwrap_or_else(|| "./docker-compose.yaml".to_string());
         println!("Initializing docker compose file at: {}", &path);
         let path = std::path::Path::new(&path);
         let prefix = path.parent().expect("failed to get parent directory");
         std::fs::create_dir_all(prefix).expect("failed to create parent directory");
 
-        let template = DockerComposeTemplate {};
+        let extractors = extractor
+            .into_iter()
+            .map(|extractor| TemplateExtractorValue {
+                service_name: extractor.replace('/', "-"),
+                docker_image: extractor,
+            })
+            .collect();
+
+        let template = DockerComposeTemplate { extractors };
         let compose_file = template.render().unwrap();
         std::fs::write(&path, compose_file).expect("failed to write docker-compose file");
     }
