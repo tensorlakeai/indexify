fc3574ececcb3ec55731dca52b95bcd812d44db0
Ali Ahmed <alahmed.se@gmail.com>
2024-02-18 21:56:56 -0800

    Add ui/build folder check

--- build.rs
+++ build.rs
@@ -1,15 +1,22 @@
 use std::error::Error;
+use std::path::Path;
 use std::process::Command;
 use vergen::EmitBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Execute the npm commands in the 'ui' directory
-    if !Command::new("sh")
-        .arg("-c")
-        .arg("cd ui && npm ci && npm run build")
-        .status()?
-        .success() {
-        return Err("Failed to execute npm commands in the 'ui' directory".into());
+    // Check if the npm build is needed by verifying the existence of the 'ui/build' directory
+    if !Path::new("ui/build").exists() {
+        println!("Building UI...");
+        // Execute the npm commands in the 'ui' directory
+        if !Command::new("sh")
+            .arg("-c")
+            .arg("cd ui && npm ci && npm run build")
+            .status()?
+            .success() {
+            return Err("Failed to execute npm commands in the 'ui' directory".into());
+        }
+    } else {
+        println!("UI build is up to date, skipping...");
     }
 
     // Continue with the rest of the build script
@@ -21,6 +28,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .all_sysinfo()
         .emit()?;
 
+    // Configure and run the tonic build for protobuf files
     tonic_build::configure()
         .out_dir("crates/indexify_proto/src/")
         .type_attribute(
@@ -34,8 +42,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .compile(
             &["proto/coordinator_service.proto", "proto/raft.proto"],
             &["proto"],
-        )
-        .unwrap();
+        )?;
 
     Ok(())
 }
