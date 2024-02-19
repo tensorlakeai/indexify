a51f602f2b467d0bc17ab366ad95ee68efeb9b18
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-19 13:32:17 -0800

    make fmt (#359)

--- build.rs
+++ build.rs
@@ -1,14 +1,17 @@
-use std::error::Error;
-use std::fs;
-use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::{
+    error::Error,
+    fs,
+    path::{Path, PathBuf},
+    process::Command,
+};
+
 use vergen::EmitBuilder;
 
-
-/// Determines if the build directory needs to be rebuilt based on file modification times or existence.
+/// Determines if the build directory needs to be rebuilt based on file
+/// modification times or existence.
 fn needs_rebuild(source_dir: &str, build_dir: &str) -> Result<bool, Box<dyn Error>> {
     let build_path = Path::new(build_dir);
-    
+
     // Check if the build directory does not exist, and return true early if so
     if !build_path.exists() {
         return Ok(true);
@@ -17,12 +20,14 @@ fn needs_rebuild(source_dir: &str, build_dir: &str) -> Result<bool, Box<dyn Erro
     // Get the modification time of the build directory
     let build_time = match build_path.metadata()?.modified() {
         Ok(time) => time,
-        Err(_) => return Ok(true), // If we can't get the modification time, assume rebuild is needed
+        Err(_) => return Ok(true), /* If we can't get the modification time, assume rebuild is
+                                    * needed */
     };
 
     let node_modules_path = Path::new(source_dir).join("node_modules");
 
-    // Recursively check if any file in the source directory is newer than the build directory, excluding node_modules
+    // Recursively check if any file in the source directory is newer than the build
+    // directory, excluding node_modules
     let mut paths_to_check = vec![PathBuf::from(source_dir)];
     while let Some(path) = paths_to_check.pop() {
         if path == node_modules_path {
@@ -54,7 +59,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             .arg("-c")
             .arg("cd ui && npm ci && npm run build")
             .status()?
-            .success() {
+            .success()
+        {
             return Err("Failed to execute npm commands in the 'ui' directory".into());
         }
     } else {
