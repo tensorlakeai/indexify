56e6e78b5b0340f09852dfd7c36abd2657874b75
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-22 19:02:37 -0800

    UI - Update getindexify version (#368)
    * udpate getindexify version
    
    * fix lint

--- ui/src/routes/Namespace/index.tsx
+++ ui/src/routes/Namespace/index.tsx
@@ -1,12 +1,10 @@
 import { IndexifyClient, Extractor, IContentMetadata, IIndex } from "getindexify";
 import { useLoaderData, LoaderFunctionArgs } from "react-router-dom";
-import { Box, Typography, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import IndexTable from "../../components/IndexTable";
 import ContentTable from "../../components/ContentTable";
 import React from "react";
 import ExtractionPoliciesTable from "../../components/ExtractionPoliciesTable";
-import CircleIcon from "@mui/icons-material/Circle";
-import { stringToColor } from "../../utils/helpers";
 import ExtractorsTable from "../../components/ExtractorsTable";
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -30,18 +28,6 @@ const NamespacePage = () => {
 
   return (
     <Stack direction="column" spacing={3}>
-      <Box display={"flex"} alignItems={"center"}>
-        <CircleIcon
-          sx={{
-            width: "30px",
-            color: stringToColor(client.namespace),
-            mr: 1,
-          }}
-        />
-        <Typography variant="h2" component="h1">
-          {client.namespace}
-        </Typography>
-      </Box>
       <ExtractionPoliciesTable
         namespace={client.namespace}
         extractionPolicies={client.extractionPolicies}
