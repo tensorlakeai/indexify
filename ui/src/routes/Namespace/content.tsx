738c74cab3acba00b28a67ce541d1f837a201a52
Lucas Jackson <lucasjackson5815@gmail.com>
2024-03-07 14:14:27 -0800

    UI - Update content retrieval with download api (#398)
    * update ui indexify and update content retrieval with download api
    
    * wip
    
    * update getindexify package
    
    * version bump

--- ui/src/routes/Namespace/content.tsx
+++ ui/src/routes/Namespace/content.tsx
@@ -1,7 +1,7 @@
 import { useLoaderData, LoaderFunctionArgs, redirect } from "react-router-dom";
 import { Typography, Stack, Breadcrumbs, Box } from "@mui/material";
-import { IndexifyClient, IContent, ITask } from "getindexify";
-import React from "react";
+import { IContentMetadata, IndexifyClient, ITask } from "getindexify";
+import React, { useEffect, useState } from "react";
 import TasksTable from "../../components/TasksTable";
 import { Link } from "react-router-dom";
 
@@ -14,72 +14,57 @@ export async function loader({ params }: LoaderFunctionArgs) {
   const tasks = await client
     .getTasks()
     .then((tasks) => tasks.filter((t) => t.content_metadata.id === contentId));
-  const content = await client.getContentById(contentId);
-  return { namespace, tasks, contentId, content };
+  const contentMetadata = await client.getContentById(contentId);
+  return { client, namespace, tasks, contentId, contentMetadata };
 }
 
 const ContentPage = () => {
-  const { namespace, tasks, contentId, content } = useLoaderData() as {
-    namespace: string;
-    tasks: ITask[];
-    contentId: string;
-    content: IContent;
-  };
+  const { client, namespace, tasks, contentId, contentMetadata } =
+    useLoaderData() as {
+      namespace: string;
+      tasks: ITask[];
+      contentId: string;
+      contentMetadata: IContentMetadata;
+      client: IndexifyClient;
+    };
 
-  function byteArrayToBlob(byteArray: number[]): Blob {
-    const arrayBuffer = new ArrayBuffer(byteArray.length);
-    const uint8Array = new Uint8Array(arrayBuffer);
-    for (let i = 0; i < byteArray.length; i++) {
-      uint8Array[i] = byteArray[i];
-    }
-    return new Blob([uint8Array]);
-  }
-
-  function bytesToString(byteArray: number[]) {
-    const chunkSize = 10000;
-    const decoder = new TextDecoder("utf-8");
-    let result = "";
-
-    for (let i = 0; i < byteArray.length; i += chunkSize) {
-      const chunk = byteArray.slice(i, i + chunkSize);
-      result += decoder.decode(new Uint8Array(chunk), { stream: true });
-    }
-
-    // Decode any remaining parts of the byteArray
-    result += decoder.decode(); // Flush the decoder's state
-    return result;
-  }
-
+  const [textContent, setTextContent] = useState("");
+  useEffect(() => {
+    client.downloadContent<string>(contentId).then((data) => {
+      setTextContent(data);
+    });
+  }, [client, contentId]);
+  
   const renderContent = () => {
-    if (content.content_type.startsWith("image")) {
-      const blob = byteArrayToBlob(content.bytes);
+    if (contentMetadata.mime_type.startsWith("image")) {
       return (
         <img
           alt="content"
-          src={URL.createObjectURL(blob)}
+          src={contentMetadata.content_url}
           width="100%"
           style={{ maxWidth: "200px" }}
           height="auto"
         />
       );
-    } else if (content.content_type.startsWith("audio")) {
-      const blob = byteArrayToBlob(content.bytes);
+    } else if (contentMetadata.mime_type.startsWith("audio")) {
       return (
         <audio controls>
-          <source src={URL.createObjectURL(blob)} type={content.content_type} />
+          <source
+            src={contentMetadata.content_url}
+            type={contentMetadata.mime_type}
+          />
           Your browser does not support the audio element.
         </audio>
       );
-    } else if (content.content_type.startsWith("video")) {
-      const blob = byteArrayToBlob(content.bytes);
+    } else if (contentMetadata.mime_type.startsWith("video")) {
       return (
         <video
-          src={URL.createObjectURL(blob)}
+          src={contentMetadata.content_url}
           controls
           style={{ width: "100%", maxWidth: "400px", height: "auto" }}
         />
       );
-    } else if (content.content_type.startsWith("text")) {
+    } else if (contentMetadata.mime_type.startsWith("text")) {
       return (
         <Box
           sx={{
@@ -87,9 +72,7 @@ const ContentPage = () => {
             overflow: "scroll",
           }}
         >
-          <Typography variant="body2">
-            {bytesToString(content.bytes)}
-          </Typography>
+          <Typography variant="body2">{textContent}</Typography>
         </Box>
       );
     }
@@ -106,7 +89,9 @@ const ContentPage = () => {
         <Typography color="text.primary">{contentId}</Typography>
       </Breadcrumbs>
       <Typography variant="h2">Content - {contentId}</Typography>
-      <Typography variant="body1">MimeType: {content.content_type}</Typography>
+      <Typography variant="body1">
+        MimeType: {contentMetadata.mime_type}
+      </Typography>
       {/* display content */}
       {renderContent()}
       {/* tasks */}
