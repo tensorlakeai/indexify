725f20899a761ef769bfad9f7f6512773a83dc8e
Lucas Jackson <lucasjackson5815@gmail.com>
2024-03-07 13:41:57 -0800

    wip

--- ui/src/routes/Namespace/content.tsx
+++ ui/src/routes/Namespace/content.tsx
@@ -1,10 +1,9 @@
 import { useLoaderData, LoaderFunctionArgs, redirect } from "react-router-dom";
 import { Typography, Stack, Breadcrumbs, Box } from "@mui/material";
 import { IContentMetadata, IndexifyClient, ITask } from "getindexify";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TasksTable from "../../components/TasksTable";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const namespace = params.namespace;
@@ -20,23 +19,28 @@ export async function loader({ params }: LoaderFunctionArgs) {
 }
 
 const ContentPage = () => {
-  const { client, namespace, tasks, contentId, contentMetadata } = useLoaderData() as {
-    namespace: string;
-    tasks: ITask[];
-    contentId: string;
-    contentMetadata: IContentMetadata;
-    client: IndexifyClient
-  };
+  const { client, namespace, tasks, contentId, contentMetadata } =
+    useLoaderData() as {
+      namespace: string;
+      tasks: ITask[];
+      contentId: string;
+      contentMetadata: IContentMetadata;
+      client: IndexifyClient;
+    };
 
-  const [textContent, setTextContent] = useState("")
-
+  const [textContent, setTextContent] = useState("");
+  useEffect(() => {
+    client.downloadContent<string>(contentId).then((data) => {
+      setTextContent(data);
+    });
+  }, [client, contentId]);
+  
   const renderContent = () => {
-    const contentURL = `${client.serviceUrl}/namespaces/${namespace}/content/${contentId}/download`
     if (contentMetadata.mime_type.startsWith("image")) {
       return (
         <img
           alt="content"
-          src={contentURL}
+          src={contentMetadata.content_url}
           width="100%"
           style={{ maxWidth: "200px" }}
           height="auto"
@@ -45,22 +49,22 @@ const ContentPage = () => {
     } else if (contentMetadata.mime_type.startsWith("audio")) {
       return (
         <audio controls>
-          <source src={contentURL} type={contentMetadata.mime_type} />
+          <source
+            src={contentMetadata.content_url}
+            type={contentMetadata.mime_type}
+          />
           Your browser does not support the audio element.
         </audio>
       );
     } else if (contentMetadata.mime_type.startsWith("video")) {
       return (
         <video
-          src={contentURL}
+          src={contentMetadata.content_url}
           controls
           style={{ width: "100%", maxWidth: "400px", height: "auto" }}
         />
       );
     } else if (contentMetadata.mime_type.startsWith("text")) {
-      axios.get(contentURL).then(res => {
-        setTextContent(String(res.data))
-      })
       return (
         <Box
           sx={{
@@ -68,9 +72,7 @@ const ContentPage = () => {
             overflow: "scroll",
           }}
         >
-          <Typography variant="body2">
-            {textContent}
-          </Typography>
+          <Typography variant="body2">{textContent}</Typography>
         </Box>
       );
     }
@@ -87,7 +89,9 @@ const ContentPage = () => {
         <Typography color="text.primary">{contentId}</Typography>
       </Breadcrumbs>
       <Typography variant="h2">Content - {contentId}</Typography>
-      <Typography variant="body1">MimeType: {contentMetadata.mime_type}</Typography>
+      <Typography variant="body1">
+        MimeType: {contentMetadata.mime_type}
+      </Typography>
       {/* display content */}
       {renderContent()}
       {/* tasks */}
