ec2d4046a4e39b676032fce4fb1d09ae10384375
Diptanu Gon Choudhury <diptanuc@gmail.com>
2024-03-08 10:28:27 -0800

    Increasing the size of the websocket frames

--- src/server.rs
+++ src/server.rs
@@ -559,7 +559,10 @@ async fn ingest_extracted_content(
     ws: WebSocketUpgrade<IngestExtractedContentResponse, IngestExtractedContent>,
     State(state): State<NamespaceEndpointState>,
 ) -> impl IntoResponse {
-    ws.on_upgrade(|socket| inner_ingest_extracted_content(socket, state))
+    // TODO - Figure out a protocol which breaks up large messages into smaller chunks and reassembles them
+    ws.map(|ws| ws.max_message_size(592323536))
+        .map(|ws| ws.max_frame_size(592323536))
+        .on_upgrade(|socket| inner_ingest_extracted_content(socket, state))
 }
 
 // Send a ping and measure how long time it takes to get a pong back
