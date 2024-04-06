f5c58c39d02aac787e7142199a5855ba4f3c0908
Maxim Kozlovsky <maxim.kozlovsky@gmail.com>
2024-04-05 21:41:46 -0400

    implement storing metadata in lancedb
    Switch to crate lancedb from vectordb
    
    Store metadata json keys as columns in lancedb.
    
    Update schema when new keys are used in metadata.

--- src/vectordbs/qdrant.rs
+++ src/vectordbs/qdrant.rs
@@ -174,7 +174,7 @@ impl VectorDb for QdrantDb {
         let points: Vec<PointId> = vec![point_id.into()];
         let _result = self
             .create_client()?
-            .overwrite_payload(&index, None, &points.into(), metadata, None)
+            .overwrite_payload(&index, None, &points.into(), metadata, None, None)
             .await
             .map_err(|e| anyhow!("unable to update metadata: {}", e.to_string()))?;
         Ok(())
