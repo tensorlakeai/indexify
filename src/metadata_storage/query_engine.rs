819fe9dd8fd5699edbdf9bc890936f581649a938
Taehoon Moon <taehoon.moon@outlook.com>
2024-04-18 13:45:11 +0900

    Add column comment support to metadata schema (#509)
    * Add column comment support to metadata schema
    
    * Update SchemaColumn column_type to use type on serde

--- src/metadata_storage/query_engine.rs
+++ src/metadata_storage/query_engine.rs
@@ -197,7 +197,7 @@ mod tests {
             namespace: ns.to_string(),
             columns: columns
                 .into_iter()
-                .map(|(name, dtype)| (name.to_string(), dtype))
+                .map(|(name, dtype)| (name.to_string(), dtype.into()))
                 .collect(),
             content_source: cs.to_string(),
             id: nanoid!(16),
@@ -208,13 +208,13 @@ mod tests {
         index_manager: Arc<T>,
     ) {
         let cols1 = vec![
-            ("id", SchemaColumnType::Text),
-            ("name", SchemaColumnType::Text),
+            ("id", SchemaColumnType::Text.into()),
+            ("name", SchemaColumnType::Text.into()),
         ];
         let schema = create_schema("test", cols1, "User");
         let cols2 = vec![
-            ("id", SchemaColumnType::Text),
-            ("foo", SchemaColumnType::Text),
+            ("id", SchemaColumnType::Text.into()),
+            ("foo", SchemaColumnType::Text.into()),
         ];
         let schema2 = create_schema("test", cols2, "User2");
         let query_engine = QueryEngine::new(index_manager, vec![schema, schema2], "test");
