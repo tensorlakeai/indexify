423fded0b916090c3111519c495b6d3555f4a2c4
Zaid Humayun <redixhumayun@gmail.com>
2024-04-19 11:03:12 +0530

    lint fix

--- src/state/store/state_machine_objects.rs
+++ src/state/store/state_machine_objects.rs
@@ -16,8 +16,16 @@ use tracing::{error, warn};
 use super::{
     requests::{RequestPayload, StateChangeProcessed, StateMachineUpdateRequest},
     serializer::JsonEncode,
-    ExecutorId, ExtractionPolicyId, ExtractorName, JsonEncoder, NamespaceName, SchemaId,
-    StateChangeId, StateMachineColumns, StateMachineError, TaskId,
+    ExecutorId,
+    ExtractionPolicyId,
+    ExtractorName,
+    JsonEncoder,
+    NamespaceName,
+    SchemaId,
+    StateChangeId,
+    StateMachineColumns,
+    StateMachineError,
+    TaskId,
 };
 use crate::state::NodeId;
 
@@ -1205,7 +1213,7 @@ impl IndexifyState {
                     self.metrics
                         .lock()
                         .unwrap()
-                        .update_task_completion(task.outcome.clone());
+                        .update_task_completion(task.outcome);
 
                     //  If the task is meant to be marked finished and has an executor id, remove it
                     // from the list of tasks assigned to an executor
