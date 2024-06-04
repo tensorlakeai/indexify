7ec001df0e6b47a5228ac066a9d9886f4e273cd5
Edwin Kys <itsedwinkys@gmail.com>
2024-06-04 12:21:12 -0500

    feat: convert float to int where .0

--- crates/indexify_internal_api/src/utils.rs
+++ crates/indexify_internal_api/src/utils.rs
@@ -14,6 +14,21 @@ pub fn convert_prost_to_serde_json(
 ) -> Result<serde_json::Value> {
     let string_value = serde_json::to_string(&prost_json)?;
     let value = serde_json::from_str(&string_value)?;
+
+    // If the value is a float and has a trailing zero, convert it to an integer.
+    // This is required because the prost_wkt_types::Value only supports floats.
+    if let serde_json::Value::Number(n) = &value {
+        if let Some(n) = &n.as_f64() {
+            let string_float = format!("{n:.1}");
+            let last_number = string_float.chars().last().unwrap();
+            if last_number == '0' {
+                let int = n.round() as i64;
+                let value = serde_json::Value::Number(int.into());
+                return Ok(value);
+            }
+        }
+    }
+
     Ok(value)
 }
 
