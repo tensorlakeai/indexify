04be0cb847c0e82b84d3b32909c818c56567d6cd
Edwin Kys <itsedwinkys@gmail.com>
2024-06-04 20:44:44 -0500

    refactor: conversion prost and serde json

--- crates/indexify_internal_api/src/utils.rs
+++ crates/indexify_internal_api/src/utils.rs
@@ -12,46 +12,30 @@ pub fn convert_serde_to_prost_json(
 pub fn convert_prost_to_serde_json(
     prost_json: prost_wkt_types::Value,
 ) -> Result<serde_json::Value> {
-    let string_value = serde_json::to_string(&prost_json)?;
-    let value = serde_json::from_str(&string_value)?;
-
-    // If the value is a float and has a trailing zero, convert it to an integer.
-    // This is required because the prost_wkt_types::Value only supports floats.
-    if let serde_json::Value::Number(n) = &value {
-        if let Some(n) = &n.as_f64() {
-            let string_float = format!("{n:.1}");
-            let last_number = string_float.chars().last().unwrap();
-            if last_number == '0' {
-                let int = n.round() as i64;
-                let value = serde_json::Value::Number(int.into());
-                return Ok(value);
-            }
-        }
+    let value = serde_json::from_str(&serde_json::to_string(&prost_json)?)?;
+    match &value {
+        serde_json::Value::Number(n) if n.as_f64().map_or(false, |n| n.fract() == 0.0) => Ok(
+            serde_json::Value::Number((n.as_f64().unwrap().round() as i64).into()),
+        ),
+        _ => Ok(value),
     }
+}
 
-    Ok(value)
+pub fn convert_map<T, V, F>(map: HashMap<String, T>, f: F) -> Result<HashMap<String, V>>
+where
+    F: Fn(T) -> Result<V>,
+{
+    map.into_iter().map(|(k, v)| Ok((k, f(v)?))).collect()
 }
 
 pub fn convert_map_serde_to_prost_json(
     map: HashMap<String, serde_json::Value>,
 ) -> Result<HashMap<String, prost_wkt_types::Value>> {
-    let mut new_map = std::collections::HashMap::new();
-    for (key, value) in map {
-        let new_value = convert_serde_to_prost_json(value)?;
-        new_map.insert(key, new_value);
-    }
-
-    Ok(new_map)
+    convert_map(map, convert_serde_to_prost_json)
 }
 
 pub fn convert_map_prost_to_serde_json(
     map: HashMap<String, prost_wkt_types::Value>,
 ) -> Result<HashMap<String, serde_json::Value>> {
-    let mut new_map = std::collections::HashMap::new();
-    for (key, value) in map {
-        let new_value = convert_prost_to_serde_json(value)?;
-        new_map.insert(key, new_value);
-    }
-
-    Ok(new_map)
+    convert_map(map, convert_prost_to_serde_json)
 }
