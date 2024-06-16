2f21e5e0c08379f2555f20e520c8a00599256cc1
Diptanu Gon Choudhury <diptanuc@gmail.com>
2024-06-15 19:33:17 -0700

    update getting started code

---
+++ examples/getting_started/example3.py
@@ -0,0 +1,24 @@
+from indexify import IndexifyClient, ExtractionGraph
+import requests
+
+client = IndexifyClient()
+
+extraction_graph_spec = """
+name: 'imageknowledgebase'
+extraction_policies:
+   - extractor: 'tensorlake/yolo-extractor'
+     name: 'object_detection'
+"""
+
+extraction_graph = ExtractionGraph.from_yaml(extraction_graph_spec)
+client.create_extraction_graph(extraction_graph)
+
+with open("sample.jpg", 'wb') as file:
+  file.write((requests.get("https://extractor-files.diptanu-6d5.workers.dev/people-standing.jpg")).content)
+content_id = client.upload_file("imageknowledgebase", "sample.jpg")
+
+client.wait_for_extraction(content_id)
+client.get_extracted_content(content_id)
+
+print(client.sql_query("select * from imageknowledgebase where object_name='skateboard';"))
+
