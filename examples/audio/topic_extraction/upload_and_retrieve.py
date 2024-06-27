3a6be44c1d97c60f4244fde417cd7e9b3f92faa4
Diptanu Gon Choudhury <diptanuc@gmail.com>
2024-06-26 23:37:25 -0700

    update topic extraction

---
+++ examples/audio/topic_extraction/upload_and_retrieve.py
@@ -0,0 +1,31 @@
+import requests
+
+from indexify import IndexifyClient
+import os
+
+client = IndexifyClient()
+
+def upload(file_url):
+    print(f"downloading file {file_url}", flush=True)
+    response = requests.get(file_url)
+    response.raise_for_status()
+    with open("audio.mp3", "wb") as f:
+        f.write(response.content)
+    content_id = client.upload_file("topic_extraction", "audio.mp3")
+    return content_id
+
+def retrieve(content_id):
+    print(f"waiting for extraction of {content_id}", flush=True)
+    client.wait_for_extraction(content_id)
+    transcription = client.get_extracted_content(content_id, "topic_extraction", "transcription")
+    print(f"transcription: \n", flush=True)
+    print(transcription)
+    summary = client.get_extracted_content(content_id, "topic_extraction", "summarization")
+    print(summary)
+    topics_extracted = client.get_extracted_content(content_id, "topic_extraction", "topics")
+
+
+if __name__ == "__main__":
+    content_id = upload("https://pub-4df2354662e34feca92dcebb4ebc8590.r2.dev/The%20Last%20Thing%20To%20Ever%20Happen%20In%20The%20Universe.mp3")
+    retrieve(content_id)
+    os.remove("audio.mp3")
