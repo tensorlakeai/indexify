481e5d0ef2da4e3befac6d383ee15abb932c67a7
Rishiraj Acharya <44090649+rishiraj@users.noreply.github.com>
2024-08-07 19:16:23 +0530

    added high quality example images and tables

--- examples/pdf/indexing_and_rag/upload_and_retrieve.py
+++ examples/pdf/indexing_and_rag/upload_and_retrieve.py
@@ -1,4 +1,4 @@
-from indexify import IndexifyClient
+from indexify import IndexifyClient, simple_directory_loader
 import requests
 from openai import OpenAI
 import tempfile
@@ -7,41 +7,41 @@ client = IndexifyClient()
 
 client_openai = OpenAI()
 
-def upload_file(url):
-    response = requests.get(url)
-    with tempfile.NamedTemporaryFile(delete=True, suffix=".pdf") as f:
-        f.write(response.content)
-        pdf_path = f.name
-        content_id = client.upload_file("rag_pipeline", pdf_path)
-        print(f"PDF uploaded with content id: {content_id}")
+# def get_page_number(content_id: str) -> int:
+#     content_metadata = client.get_content_metadata(content_id)
+#     page_number = content_metadata["extracted_metadata"]["metadata"]['page_num']
+#     return page_number
 
-    client.wait_for_extraction(content_id)
+# def get_context(question: str, index: str, top_k=5):
+#     results = client.search_index(name=index, query=question, top_k=top_k)
+#     context = ""
+#     for result in results:
+#         # Search result returns the chunk id. Chunks are derived from extracted pages, which are 
+#         # the 'parent', so we grab the parent id and get the content metadata of the page. The page numbers
+#         # are stored in the extracted metadata of the pages.
+#         parent_id = result['content_metadata']['parent_id']
+#         page_number = get_page_number(parent_id)
+#         context = context + f"content id: {result['content_id']} \n\n page number: {page_number} \n\n passage: {result['text']}\n"
+#     return context
 
-def get_page_number(content_id: str) -> int:
-    content_metadata = client.get_content_metadata(content_id)
-    page_number = content_metadata["extracted_metadata"]["metadata"]['page_num']
-    return page_number
+# def create_prompt(question, context):
+#     return f"""Answer the question based only on the following context, which can include text and tables.
+#     Mention the content ids and page numbers as citation at the end of the response, format -
+#     Citations: 
+#     Content ID: <> Page Number <>.
 
-def get_context(question: str, index: str, top_k=3):
+#     question: {question}
+#     context: {context}"""
+
+def get_context(question: str, index: str, top_k=5):
     results = client.search_index(name=index, query=question, top_k=top_k)
     context = ""
     for result in results:
-        # Search result returns the chunk id. Chunks are derived from extracted pages, which are 
-        # the 'parent', so we grab the parent id and get the content metadata of the page. The page numbers
-        # are stored in the extracted metadata of the pages.
-        parent_id = result['content_metadata']['parent_id']
-        page_number = get_page_number(parent_id)
-        context = context + f"content id: {result['content_id']} \n\n page number: {page_number} \n\n passage: {result['text']}\n"
+        context = context + f"content id: {result['content_id']} \n\n passage: {result['text']}\n"
     return context
 
 def create_prompt(question, context):
-    return f"""Answer the question, based on the context.
-    Mention the content ids and page numbers as citation at the end of the response, format -
-    Citations: 
-    Content ID: <> Page Number <>.
-
-    question: {question}
-    context: {context}"""
+    return f"Answer the question based only on the following context, which can include text and tables: \n question: {question} \n context: {context}"
 
 def answer_question(question):
     context = get_context(question, "rag_pipeline.chunk_embedding.embedding")
@@ -60,11 +60,32 @@ def answer_question(question):
 
 # Example usage
 if __name__ == "__main__":
-    pdf_url = "https://proceedings.neurips.cc/paper_files/paper/2017/file/3f5ee243547dee91fbd053c1c4a845aa-Paper.pdf"
+    pdf_urls = [
+        "https://arxiv.org/pdf/2304.08485.pdf",
+        "https://arxiv.org/pdf/0910.2029.pdf",
+        "https://arxiv.org/pdf/2402.01968.pdf",
+        "https://arxiv.org/pdf/2401.13138.pdf",
+        "https://arxiv.org/pdf/2402.03578.pdf",
+        "https://arxiv.org/pdf/2309.07864.pdf",
+        "https://arxiv.org/pdf/2401.03568.pdf",
+        "https://arxiv.org/pdf/2312.10256.pdf",
+        "https://arxiv.org/pdf/2312.01058.pdf",
+        "https://arxiv.org/pdf/2402.01680.pdf",
+        "https://arxiv.org/pdf/2403.07017.pdf"
+    ]
 
-    upload_file(pdf_url)
+    content_ids = simple_directory_loader(
+        client=client,
+        extraction_graph="rag_pipeline",
+        directory="pdfs",
+        file_extensions=[".pdf"],
+        download_urls=pdf_urls,
+        wait_for_extraction=True
+    )
+
+    print(f"Processed {len(content_ids)} documents")
     
-    question = "What was the hardware the model was trained on and how long it was trained?"
+    question = "What is the performance of LLaVa across across multiple image domains / subjects?"
     answer = answer_question(question)
     print(f"Question: {question}")
     print(f"Answer: {answer}")
