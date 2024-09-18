5e927cc1b3898c9990e8ce01da439c8825513d09
Sarma Tangirala <tvssarma.omega9@gmail.com>
2024-09-17 20:02:30 -0700

    PR comment

--- python-sdk/indexify/functions_sdk/graph.py
+++ python-sdk/indexify/functions_sdk/graph.py
@@ -24,6 +24,10 @@ from typing_extensions import get_args, get_origin
 
 from .cbor_serializer import CborSerializer
 from .data_objects import BaseData, RouterOutput
+from .graph_validation import (
+    validate_node,
+    validate_route,
+)
 from .indexify_functions import (
     IndexifyFunction,
     IndexifyFunctionWrapper,
@@ -108,31 +112,9 @@ class Graph:
     def add_node(
         self, indexify_fn: Union[Type[IndexifyFunction], Type[IndexifyRouter]]
     ) -> "Graph":
-        # Validation
-        if not (
-            indexify_fn.__name__ == "IndexifyFn" or
-            indexify_fn.__name__ == "IndexifyRo"
-        ):
-            raise Exception(
-                f"Unable to add node of type `{indexify_fn.__name__}`. "
-                f"Required, `IndexifyFunction` or `IndexifyRouter`"
-            )
 
-        signature = inspect.signature(indexify_fn.run)
+        validate_node(indexify_fn=indexify_fn)
 
-        for param in signature.parameters.values():
-            if param.annotation == inspect.Parameter.empty:
-                raise Exception(
-                    f"Input param {param.name} in {indexify_fn.name} has empty"
-                    f" type annotation"
-                )
-
-        if signature.return_annotation == inspect.Signature.empty:
-            raise Exception(
-                f"Function {indexify_fn.name} has empty return type annotation"
-            )
-
-        # main
         if indexify_fn.name in self.nodes:
             return self
 
@@ -143,43 +125,8 @@ class Graph:
         self, from_node: Type[IndexifyRouter], to_nodes: List[Type[IndexifyFunction]]
     ) -> "Graph":
 
-        # Validation
-        signature = inspect.signature(from_node.run)
+        validate_route(from_node=from_node, to_nodes=to_nodes)
 
-        if signature.return_annotation == inspect.Signature.empty:
-            raise Exception(
-                f"Function {from_node.name} has empty return type annotation"
-            )
-
-        # We lose the exact type string when the object is created
-        source = inspect.getsource(from_node.run)
-
-        union_pattern = r'Union\[((?:\w+(?:,\s*)?)+)\]'
-        union_match = re.search(union_pattern, source)
-
-        src_route_nodes = None
-        if union_match:
-            # nodes = re.findall(r'\w+', match.group(1))
-            src_route_nodes = [node.strip() for node in union_match.group(1).split(',')]
-            if len(src_route_nodes) <= 1:
-                raise Exception(
-                    f"Invalid router for {from_node.name}, lte 1 route."
-                )
-        else:
-            raise Exception(
-                f"Invalid router for {from_node.name}, cannot find output nodes"
-            )
-
-        to_node_names = [i.name for i in to_nodes]
-
-        for src_node in src_route_nodes:
-            if src_node not in to_node_names:
-                raise Exception(
-                    f"Unable to find {src_node} in to_nodes "
-                    f"{to_node_names}"
-                )
-
-        # main
         print(
             f"Adding router {from_node.name} to nodes {[node.name for node in to_nodes]}"
         )
