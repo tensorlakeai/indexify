3f4bea2bd5493731e013d41f15b6b4aa089da3b2
Adithya Krishna <aadithya794@gmail.com>
2024-09-24 01:15:47 +0530

    chore: remove old files
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- ui/src/routes/Namespace/index.ts
+++ ui/src/routes/Namespace/index.ts
@@ -1,7 +1,5 @@
-// export { default as ExtractionPolicyPage } from "./ExtractionPolicyPage";
-// export { default as IndividualContentPage } from "./IndividualContentPage";
 export { default as NamespacesPage } from "./NamespacesPage";
 export { default as ComputeGraphsPage } from "./ComputeGraphsPage";
 export { default as IndividualComputeGraphPage } from "./IndividualComputeGraphPage";
-// export { default as IndexesPage } from "./IndexesPage";
+export { default as IndividualInvocationPage } from "./IndividualInvocationPage";
 
