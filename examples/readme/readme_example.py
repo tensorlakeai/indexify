53087d2531be25fe364dfa7fd844f2115606313f
Diptanu Gon Choudhury <diptanuc@gmail.com>
2024-09-27 22:50:16 -0700

    update pyproject

--- examples/readme/readme_example.py
+++ examples/readme/readme_example.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
-from indexify import indexify_function, Graph
-from typing import List
+from indexify import indexify_function, indexify_router, Graph
+from typing import List, Union
 
 @indexify_function()
 def generate_sequence(a: int) -> List[int]:
@@ -11,27 +11,33 @@ class Sum(BaseModel):
 
 @indexify_function(accumulate=Sum)
 def sum_all_numbers(sum: Sum, val: int) -> Sum:
-    val += sum.val
+    val = sum.val + val
     return Sum(val=val)
 
 @indexify_function()
-def square_of_sum(sum: Sum) -> int:
+def squared(sum: Sum) -> int:
     return sum.val * sum.val
 
+@indexify_function()
+def tripled(sum: Sum) -> int:
+    return sum.val * sum.val * sum.val
 
-def create_graph() -> Graph:
-    g = Graph(name="sequence_summer", start_node=generate_sequence, description="Simple Sequence Summer")
-    g.add_edge(generate_sequence, sum_all_numbers)
-    g.add_edge(sum_all_numbers, square_of_sum)
-    return g
+@indexify_router()
+def dynamic_router(val: Sum) -> List[Union[squared, tripled]]:
+    if val.val % 2:
+        return [squared]
+    return [tripled]
 
+from indexify import create_client 
 
-if __name__ == "__main__":
-    from indexify import create_client 
-    g = create_graph()
-    client = create_client(local=True)
-    client.register_compute_graph(g)
+g = Graph(name="sequence_summer", start_node=generate_sequence, description="Simple Sequence Summer")
+g.add_edge(generate_sequence, sum_all_numbers)
+g.add_edge(sum_all_numbers, dynamic_router)
+g.route(dynamic_router, [squared, tripled])
 
-    invocation_id = client.invoke_graph_with_object("sequence_summer", a=10)
-    result = client.graph_outputs("sequence_summer", invocation_id,  "square_of_sum")
-    print(result)
+client = create_client()
+client.register_compute_graph(g)
+
+invocation_id = client.invoke_graph_with_object("sequence_summer", block_until_done=True, a=10)
+result = client.graph_outputs("sequence_summer", invocation_id, "squared")
+print(result)
