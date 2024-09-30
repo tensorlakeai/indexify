bf04bc0abf926beeb405750752baadd8f5978e35
diptanu <diptanuc@gmail.com>
2024-09-30 20:58:38 +0000

    showing a distributed map

---
+++ examples/readme/distributed_map.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from indexify import indexify_function, indexify_router, Graph
+from typing import List, Union
+
+@indexify_function()
+def generate_sequence(a: int) -> List[int]:
+    return [i for i in range(a)]
+
+@indexify_function()
+def squared(x: int) -> int:
+    return x * x
+
+if __name__ == '__main__':
+    g = Graph(name="sequence_summer", start_node=generate_sequence, description="Simple Sequence Summer")
+    g.add_edge(generate_sequence, squared)
+
+    from indexify import create_client
+
+    client = create_client()
+    client.register_compute_graph(g)
+
+    invocation_id = client.invoke_graph_with_object("sequence_summer",
+            block_until_done=True, a=90)
+    result = client.graph_outputs("sequence_summer", invocation_id, "squared")
+    print(result)
