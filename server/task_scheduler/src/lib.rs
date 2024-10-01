3059278262443e46e6894fcb33fcc28689d25390
Miguel Hernández <miguel.hernandez@hashicorp.com>
2024-10-01 21:59:18 +0200

    feat: filter incompatible executors (#898)
    * add runtime_information to graph dto
    
    * add runtime_information to graph data model
    
    * filter out incompatible executors
    
    * be better at rust
    
    * make fmt
    
    * cleanup PR
    
    * address code review feedback

--- server/task_scheduler/src/lib.rs
+++ server/task_scheduler/src/lib.rs
@@ -1,10 +1,10 @@
 use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
-use data_model::{ExecutorId, Node, ReduceTask, Task};
+use data_model::{ExecutorId, Node, ReduceTask, RuntimeInformation, Task};
 use rand::seq::SliceRandom;
 use state_store::{requests::TaskPlacement, IndexifyState};
-use tracing::info;
+use tracing::{error, info};
 
 pub mod task_creator;
 
@@ -45,7 +45,7 @@ impl TaskScheduler {
                 .nodes
                 .get(&task.compute_fn_name)
                 .ok_or(anyhow!("Compute fn not found"))?;
-            let executor_ids = self.filter_executors(&compute_fn)?;
+            let executor_ids = self.filter_executors(&compute_fn, &cg.runtime_information)?;
             let executor_id = executor_ids.choose(&mut rand::thread_rng());
             if let Some(executor_id) = executor_id {
                 info!("Assigning task {:?} to executor {:?}", task.id, executor_id);
@@ -58,14 +58,37 @@ impl TaskScheduler {
         Ok(task_allocations)
     }
 
-    fn filter_executors(&self, node: &Node) -> Result<Vec<ExecutorId>> {
+    fn filter_executors(
+        &self,
+        node: &Node,
+        runtime_information: &RuntimeInformation,
+    ) -> Result<Vec<ExecutorId>> {
         let executors = self.indexify_state.reader().get_all_executors()?;
         let mut filtered_executors = Vec::new();
 
         for executor in &executors {
+            let raw_minor_version = executor.labels.get("python_minor_version");
+            if let Some(minor_version) = raw_minor_version {
+                let minor_version = serde_json::from_value::<u8>(minor_version.clone());
+                if let Ok(minor_version) = minor_version {
+                    if minor_version != runtime_information.minor_version {
+                        info!(
+                            "skipping executor {} because python version does not match",
+                            executor.id
+                        );
+                        continue;
+                    }
+                    info!("executor {} has python_minor_version label", executor.id);
+                } else {
+                    error!("failed to parse python_minor_version label");
+                    continue;
+                }
+            }
+
             if executor.image_name != node.image_name() {
                 continue;
             }
+
             if node.matches_executor(executor) {
                 filtered_executors.push(executor.id.clone());
             }
