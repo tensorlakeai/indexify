5e7a2ef7b06b24f08d107d55b0d8d7280ece220c
Adithya Krishna <aadithya794@gmail.com>
2024-10-07 13:14:17 +0530

    chore: group compute functions by name
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- server/ui/src/components/tables/InvocationOutputTable.tsx
+++ server/ui/src/components/tables/InvocationOutputTable.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
   Table,
   TableBody,
   TableCell,
@@ -12,6 +15,7 @@ import {
   Box,
   Alert,
 } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 interface Output {
   compute_fn: string;
@@ -44,40 +48,61 @@ const InvocationOutputTable: React.FC<InvocationOutputTableProps> = ({ indexifyS
     };
 
     fetchOutputs();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [invocationId, namespace, computeGraph]);
+  }, [indexifyServiceURL, invocationId, namespace, computeGraph]);
 
   if (!outputs || outputs.length === 0) {
-      return (
-        <Box mt={2} mb={2}>
-          <Alert variant="outlined" severity="info">
-            No Outputs Found
-          </Alert>
-        </Box>
-      );
+    return (
+      <Box mt={2} mb={2}>
+        <Alert variant="outlined" severity="info">
+          No Outputs Found
+        </Alert>
+      </Box>
+    );
+  }
+
+  // Group outputs by compute_fn
+  const groupedOutputs = outputs.reduce((acc, output) => {
+    if (!acc[output.compute_fn]) {
+      acc[output.compute_fn] = [];
     }
+    acc[output.compute_fn].push(output);
+    return acc;
+  }, {} as Record<string, Output[]>);
 
   return (
     <Box sx={{ width: '100%', mt: 2 }}>
       <Typography variant="h6" gutterBottom>Outputs for Invocation</Typography>
-      <TableContainer component={Paper} sx={{boxShadow: "0px 0px 2px 0px rgba(51, 132, 252, 0.5) inset"}}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Compute Function</TableCell>
-              <TableCell>ID</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {outputs.map((output, index) => (
-              <TableRow key={index}>
-                <TableCell>{output.compute_fn}</TableCell>
-                <TableCell>{output.id}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      {Object.entries(groupedOutputs).map(([computeFn, outputs], index) => (
+        <Accordion key={index} defaultExpanded>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`panel${index}-content`}
+            id={`panel${index}-header`}
+          >
+            <Typography>Compute Function - {computeFn} ({outputs.length} outputs)</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <TableContainer component={Paper} sx={{boxShadow: "0px 0px 2px 0px rgba(51, 132, 252, 0.5) inset",}} elevation={0}>
+              <Table size="small">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Compute Function</TableCell>
+                    <TableCell>ID</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {outputs.map((output, idx) => (
+                    <TableRow key={idx}>
+                      <TableCell>{output.compute_fn}</TableCell>
+                      <TableCell>{output.id}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </Box>
   );
 };
