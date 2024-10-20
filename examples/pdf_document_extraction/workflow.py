82cb1cf23a3fe14db9533a91120fd08f5494f466
Diptanu Gon Choudhury <diptanuc@gmail.com>
2024-10-20 15:49:15 -0700

    update example

--- examples/pdf_document_extraction/workflow.py
+++ examples/pdf_document_extraction/workflow.py
@@ -52,15 +52,3 @@ if __name__ == "__main__":
     )
     print(f"Invocation ID: {invocation_id}")
 
-    from chromadb  import HttpClient, QueryResult
-    client = HttpClient(host="localhost", port=8000)
-    from sentence_transformers import SentenceTransformer
-    
-    # Query VectorDB for similar text
-    text_collection = client.get_collection("text_embeddings-1")
-    query_embeddings = SentenceTransformer('Alibaba-NLP/gte-base-en-v1.5', trust_remote_code=True).encode(["transformers"])
-    result: QueryResult = text_collection.query(query_embeddings, n_results=5, )
-    documents = result["documents"][0]
-    distances = result["distances"][0]
-    for i, document in enumerate(documents):
-        print(f"document {document}, score: {distances[i]}")
