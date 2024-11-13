0726e8ab3cc6448fd71f58709a915eccf156ec80
Default28 <soumyasaurav@gmail.com>
2024-11-13 14:02:15 +0530

    [WIP] removing MsgpackSerializer from object_serializer.py

--- python-sdk/indexify/functions_sdk/object_serializer.py
+++ python-sdk/indexify/functions_sdk/object_serializer.py
@@ -2,17 +2,10 @@ from typing import Any, List
 
 import cloudpickle
 import jsonpickle
-import msgpack
-from pydantic import BaseModel
-
-from .data_objects import IndexifyData
-
 
 def get_serializer(serializer_type: str) -> Any:
     if serializer_type == "cloudpickle":
         return CloudPickleSerializer()
-    elif serializer_type == "msgpack":
-        return MsgPackSerializer()
     elif serializer_type == "json":
         return JsonSerializer()
     raise ValueError(f"Unknown serializer type: {serializer_type}")
@@ -52,29 +45,3 @@ class CloudPickleSerializer:
     @staticmethod
     def deserialize_list(data: bytes) -> List[Any]:
         return cloudpickle.loads(data)
-
-
-class MsgPackSerializer:
-    @staticmethod
-    def serialize(data: Any) -> bytes:
-        if (
-            isinstance(data, type)
-            and issubclass(data, BaseModel)
-            or isinstance(data, BaseModel)
-        ):
-            return msgpack.packb(data.model_dump())
-        return msgpack.packb(data)
-
-    @staticmethod
-    def deserialize(data: bytes) -> IndexifyData:
-        cached_output = msgpack.unpackb(data)
-        return IndexifyData(**cached_output)
-
-    @staticmethod
-    def serialize_list(data: List[IndexifyData]) -> bytes:
-        data = [item.model_dump() for item in data]
-        return msgpack.packb(data)
-
-    @staticmethod
-    def deserialize_list(data: bytes) -> List[IndexifyData]:
-        return [IndexifyData(**item) for item in msgpack.unpackb(data)]
