fbae4de96060e9811273be952cd2228ef729816f
Diptanu Gon Choudhury <diptanuc@gmail.com>
2024-11-17 23:32:51 -0800

    fix remote graph

--- python-sdk/indexify/functions_sdk/graph_definition.py
+++ python-sdk/indexify/functions_sdk/graph_definition.py
@@ -49,3 +49,11 @@ class ComputeGraphMetadata(BaseModel):
 
     def get_input_payload_serializer(self):
         return get_serializer(self.start_node.compute_fn.encoder)
+    
+    def get_input_encoder(self) -> str:
+        if self.start_node.compute_fn:
+            return self.start_node.compute_fn.encoder
+        elif self.start_node.dynamic_router:
+            return self.start_node.dynamic_router.encoder
+        
+        raise ValueError("start node is not set on the graph")
