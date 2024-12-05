2f7d298eea560e723a4fc1cc3f1fd184dec540d2
Diptanu Gon Choudhury <diptanuc@gmail.com>
2024-12-05 15:28:18 -0800

    update python sdk version

--- python-sdk/indexify/executor/agent.py
+++ python-sdk/indexify/executor/agent.py
@@ -299,7 +299,7 @@ class ExtractorAgent:
                 image_version=image_version,
                 labels=runtime_probe.labels,
             ).model_dump()
-            logging.info("registering_executor", executor_id=self._executor_id)
+            logging.info("registering_executor", executor_id=self._executor_id, url=url)
             try:
                 async with get_httpx_client(self._config_path, True) as client:
                     async with aconnect_sse(
