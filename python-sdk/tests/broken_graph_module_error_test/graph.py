76d6fa9eefb317412e1bbfc7ce9e77662a4f5623
Sarma Tangirala <sarma@tensorlake.ai>
2024-12-31 09:56:48 -0800

    Add test for broken 1p import

---
+++ python-sdk/tests/broken_graph_module_error_test/graph.py
@@ -0,0 +1,38 @@
+import sys
+import unittest
+
+from indexify import RemoteGraph
+from indexify.functions_sdk.data_objects import File
+from indexify.functions_sdk.graph import Graph
+from indexify.functions_sdk.indexify_functions import indexify_function
+
+from extractors import extractor_a, extractor_c
+
+
+def create_broken_graph():
+    g = Graph(
+        "broken-graph-without-dep-registered",
+        start_node=extractor_a,
+    )
+
+    # Parse the PDF which was downloaded
+    g.add_edge(extractor_a, extractor_c)
+    return g
+
+
+class TestBrokenGraphs(unittest.TestCase):
+    def test_broken_graph(self):
+        g = create_broken_graph()
+        g = RemoteGraph.deploy(g=g)
+
+        self.assertRaises(
+            Exception,
+            g.run(
+                block_until_done=True,
+                a=10,
+            ),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
