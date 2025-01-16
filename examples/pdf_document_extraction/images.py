294153c987903f54ac0599af9f3360c0ef3c3a2f
Julio Martinez <julio@tensorflow.ai>
2025-01-16 08:20:15 -0800

    Updates to document extraction example.

--- examples/pdf_document_extraction/images.py
+++ examples/pdf_document_extraction/images.py
@@ -3,14 +3,12 @@ from tensorlake import Image
 http_client_image = (
     Image()
     .name("tensorlake/pdf-blueprint-download")
-    .base_image(f"python:3.11-slim-bookworm")
     .run("pip install httpx")
 )
 
 chroma_image = (
     Image()
     .name("tensorlake/blueprints-chromadb")
-    .base_image(f"python:3.11-slim-bookworm")
     .run("pip install chromadb")
     .run("pip install pillow")
 )
@@ -18,7 +16,6 @@ chroma_image = (
 st_image = (
     Image()
     .name("tensorlake/pdf-blueprint-st")
-    .base_image("pytorch/pytorch:2.4.1-cuda11.8-cudnn9-runtime")
     .run("pip install sentence-transformers")
     .run("pip install langchain")
     .run("pip install pillow")
@@ -30,16 +27,15 @@ st_image = (
 lance_image = (
     Image()
     .name("tensorlake/pdf-blueprint-lancdb")
-    .base_image(f"python:3.11-slim-bookworm")
     .run("pip install lancedb")
 )
 
-inkwell_image_gpu = (
+parser_image = (
     Image()
-    .name("tensorlake/pdf-blueprint-pdf-parser-gpu")
-    .base_image("pytorch/pytorch:2.4.1-cuda11.8-cudnn9-runtime")
+    .name("tensorlake/pdf-blueprint-pdf-parser")
     .run("apt update")
     .run("apt install -y libgl1-mesa-glx git g++")
+    .run("pip install torch torchaudio torchvideo")
     .run("pip install git+https://github.com/facebookresearch/detectron2.git@v0.6")
     .run("apt install -y tesseract-ocr")
     .run("apt install -y libtesseract-dev")
@@ -47,3 +43,18 @@ inkwell_image_gpu = (
     .run('pip install docling')
     .run("pip install elastic-transport")
 )
+
+
+# parser_image = (
+#     Image()
+#     .name("tensorlake/pdf-blueprint-pdf-parser")
+#     .base_image("pytorch/pytorch:2.4.1-cuda11.8-cudnn9-runtime")
+#     .run("apt update")
+#     .run("apt install -y libgl1-mesa-glx git g++")
+#     .run("pip install git+https://github.com/facebookresearch/detectron2.git@v0.6")
+#     .run("apt install -y tesseract-ocr")
+#     .run("apt install -y libtesseract-dev")
+#     .run('pip install "py-inkwell[inference]"')
+#     .run('pip install docling')
+#     .run("pip install elastic-transport")
+# )
