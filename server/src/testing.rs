55f4d566f2f05433a6cd9a947803cee8a0b02769
Diptanu Gon Choudhury <diptanuc@gmail.com>
2025-03-09 04:28:32 -0700

    fix tests

--- server/src/testing.rs
+++ server/src/testing.rs
@@ -228,7 +228,7 @@ impl TestExecutor<'_> {
             .in_memory_state
             .read()
             .await
-            .active_tasks_for_executor(&self.executor.id.get(), 100);
+            .active_tasks_for_executor(&self.executor.id.get());
 
         Ok(tasks.into_iter().map(|task| (*task).clone()).collect())
     }
