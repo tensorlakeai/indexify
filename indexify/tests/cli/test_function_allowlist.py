da4404b48e331ad0fd01d8befa3bcda504ec2c81
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2025-03-28 11:49:47 -0400

    test: improving allow list executor test

--- indexify/tests/cli/test_function_allowlist.py
+++ indexify/tests/cli/test_function_allowlist.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 import unittest
 from typing import List, Optional
 
@@ -21,6 +22,8 @@ dev_mode_executor_pid: Optional[int] = None
 function_a_executor_pid: Optional[int] = None
 # This Executor can only run function_b.
 function_b_executor_pid: Optional[int] = None
+# This Executor can only run function_c any version.
+function_c_executor_pid: Optional[int] = None
 
 
 @tensorlake_function()
@@ -29,7 +32,7 @@ def get_dev_mode_executor_pid() -> int:
 
 
 @tensorlake_function()
-def function_a() -> str:
+def function_a() -> int:
     global dev_mode_executor_pid
     global function_a_executor_pid
     global function_b_executor_pid
@@ -40,13 +43,11 @@ def function_a() -> str:
         raise Exception(
             f"function_a Executor PID {current_executor_pid} is not in the allowlist: {allowed_executor_pids}"
         )
-    return "success"
+    return current_executor_pid
 
 
 @tensorlake_function()
-def function_b(_: str) -> str:
-    global dev_mode_executor_pid
-    global function_a_executor_pid
+def function_b(_: str) -> int:
     global function_b_executor_pid
 
     current_executor_pid: int = executor_pid()
@@ -55,27 +56,42 @@ def function_b(_: str) -> str:
         raise Exception(
             f"function_b Executor PID {current_executor_pid} is not in the allowlist: {allowed_executor_pids}"
         )
-    return "success"
+    return current_executor_pid
 
 
 @tensorlake_function()
-def function_dev(_: str) -> str:
+def function_c(_: str) -> int:
+    global function_c_executor_pid
+
+    current_executor_pid: int = executor_pid()
+    allowed_executor_pids: List[int] = [function_c_executor_pid, dev_mode_executor_pid]
+    if current_executor_pid not in allowed_executor_pids:
+        raise Exception(
+            f"function_c Executor PID {current_executor_pid} is not in the allowlist: {allowed_executor_pids}"
+        )
+    return current_executor_pid
+
+
+@tensorlake_function()
+def function_dev(_: str) -> int:
     current_executor_pid: int = executor_pid()
     allowed_executor_pids: List[int] = [dev_mode_executor_pid]
     if current_executor_pid not in allowed_executor_pids:
         raise Exception(
             f"function_dev Executor PID {current_executor_pid} is not in the allowlist: {allowed_executor_pids}"
         )
-    return "success"
+    return current_executor_pid
 
 
 class TestFunctionAllowlist(unittest.TestCase):
     def test_function_routing(self):
+        graph_name = test_graph_name(self)
+        version = str(time.time())
         graph = Graph(
-            name=test_graph_name(self),
+            name=graph_name + "_dev",
             description="test",
             start_node=get_dev_mode_executor_pid,
-            version="1.0",
+            version=version,
         )
         graph = RemoteGraph.deploy(graph, additional_modules=[testing])
 
@@ -91,9 +107,9 @@ class TestFunctionAllowlist(unittest.TestCase):
                 "--function",
                 function_uri(
                     "default",
-                    "test_different_executors_run_different_functions",
+                    graph_name,
                     "function_a",
-                    "2.0",
+                    version,
                 ),
                 "--ports",
                 "60000",
@@ -113,9 +129,9 @@ class TestFunctionAllowlist(unittest.TestCase):
                     "--function",
                     function_uri(
                         "default",
-                        "test_different_executors_run_different_functions",
+                        graph_name,
                         "function_b",
-                        "2.0",
+                        version,
                     ),
                     "--ports",
                     "60001",
@@ -130,30 +146,87 @@ class TestFunctionAllowlist(unittest.TestCase):
                 print(f"Started Executor B with PID: {function_b_executor_pid}")
                 wait_executor_startup(7002)
 
-                graph = Graph(
-                    name=test_graph_name(self),
-                    description="test",
-                    start_node=function_a,
-                    version="2.0",
-                )
-                graph.add_edge(function_a, function_b)
-                graph.add_edge(function_b, function_dev)
-                graph = RemoteGraph.deploy(graph, additional_modules=[testing])
-                # As invocations might land on dev Executor, we need to run the graph multiple times
-                # to ensure that we catch wrong routing to Executor A or B if it ever happens.
-                for _ in range(10):
-                    invocation_id = graph.run(block_until_done=True)
-                    output = graph.output(invocation_id, "function_a")
-                    self.assertEqual(len(output), 1)
-                    self.assertEqual(output[0], "success")
+                with ExecutorProcessContextManager(
+                    [
+                        "--function",
+                        function_uri(
+                            "default",
+                            graph_name,
+                            "function_c",
+                        ),
+                        "--ports",
+                        "60003",
+                        "60004",
+                        "--monitoring-server-port",
+                        "7003",
+                    ]
+                ) as executor_c:
+                    executor_c: subprocess.Popen
+                    global function_c_executor_pid
+                    function_c_executor_pid = executor_c.pid
+                    print(f"Started Executor C with PID: {function_c_executor_pid}")
+                    wait_executor_startup(7002)
 
-                    output = graph.output(invocation_id, "function_b")
-                    self.assertEqual(len(output), 1)
-                    self.assertEqual(output[0], "success")
+                    graph = Graph(
+                        name=graph_name,
+                        description="test",
+                        start_node=function_a,
+                        version=version,
+                    )
+                    graph.add_edge(function_a, function_b)
+                    graph.add_edge(function_b, function_c)
+                    graph.add_edge(function_c, function_dev)
+                    graph = RemoteGraph.deploy(graph, additional_modules=[testing])
 
-                    output = graph.output(invocation_id, "function_dev")
-                    self.assertEqual(len(output), 1)
-                    self.assertEqual(output[0], "success")
+                    invocations_per_pid = {}
+
+                    # As invocations might land on dev Executor, we need to run the graph multiple times
+                    # to ensure that we land on all executors.
+                    for _ in range(200):
+                        invocation_id = graph.run(block_until_done=True)
+                        output = graph.output(invocation_id, "function_a")
+                        self.assertEqual(len(output), 1)
+                        invocations_per_pid[output[0]] = (
+                            invocations_per_pid.get(output[0], 0) + 1
+                        )
+
+                        output = graph.output(invocation_id, "function_b")
+                        self.assertEqual(len(output), 1)
+                        invocations_per_pid[output[0]] = (
+                            invocations_per_pid.get(output[0], 0) + 1
+                        )
+                        output = graph.output(invocation_id, "function_c")
+                        self.assertEqual(len(output), 1)
+                        invocations_per_pid[output[0]] = (
+                            invocations_per_pid.get(output[0], 0) + 1
+                        )
+
+                        output = graph.output(invocation_id, "function_dev")
+                        self.assertEqual(len(output), 1)
+                        invocations_per_pid[output[0]] = (
+                            invocations_per_pid.get(output[0], 0) + 1
+                        )
+
+                        # Check that all invocations have been routed to the correct executors
+                        # at least once.
+                        if len(invocations_per_pid) == 4:
+                            break
+
+                executor_names = {
+                    dev_mode_executor_pid: "executor_dev",
+                    function_a_executor_pid: "executor_a",
+                    function_b_executor_pid: "executor_b",
+                    function_c_executor_pid: "executor_c",
+                }
+                formatted_invocations = {
+                    executor_names.get(
+                        pid, f"unknown_executor_{pid}"
+                    ): invocations_per_pid.get(pid, 0)
+                    for pid in executor_names.keys()
+                }
+                assert (
+                    len(invocations_per_pid) == 4
+                ), f"Not all executors were used: {formatted_invocations}"
 
 
 if __name__ == "__main__":
