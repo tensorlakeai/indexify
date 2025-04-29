5998438a33c89406f72aad53be0b8e137debea9c
Eugene Batalov <eugene@tensorlake.ai>
2025-04-29 12:56:03 +0100

    Fix and extend CI test runner

--- indexify/tests/run_tests.sh
+++ indexify/tests/run_tests.sh
@@ -7,25 +7,32 @@ if [[ -z "$INDEXIFY_URL" ]]; then
     exit 1
 fi
 
+tests_exit_code=0
+
 run_test_suite() {
   local test_files=$1
   local test_suite_name=$2
+  local test_suite_exit_code=0
 
-  # Run each test file one by one sequentially. Returns non zero status
-  # code if any of the test commands return non zero status code. Doesn't
+  # Run each test file one by one sequentially. Set $tests_exit_code to non zero
+  # value if any of the test commands return non zero status code. Don't
   # stop if a test command fails.
-  #
-  # Run the tests from Indexify pyenv so Executor and SDK client are in the same pyenv.
-  echo "$test_files" | xargs -L1 poetry run python
-  test_suite_exit_code=$?
-  if [ $test_suite_exit_code -ne 0 ]; then
-    echo "One or more tests failed in ${test_suite_name} test suite. Please check output log for details."
-  fi
-  return $test_suite_exit_code
+  for test_file in $test_files; do
+    echo "Running $test_file for $test_suite_name test suite"
+    poetry run python $test_file
+    local test_file_exit_code=$?
+    if [ $test_file_exit_code -ne 0 ]; then
+      echo "One or more tests failed in $test_file for $test_suite_name test suite." | tee -a $summary_file
+    fi
+    tests_exit_code=$((tests_exit_code || test_file_exit_code))
+  done
 }
 
 # cd to the script's directory.
 cd "$(dirname "$0")"
+
+summary_file=".run_tests_summary.txt"
+rm -f $summary_file
 
 # Indexify tests.
 indexify_test_files=$(find . -name 'test_*.py')
@@ -33,14 +40,13 @@ indexify_test_files=$(find . -name 'test_*.py')
 tensorlake_sdk_test_files=$(find ../../tensorlake/tests/tensorlake -name 'test_*.py')
 
 run_test_suite "$indexify_test_files" "Indexify"
-tests_exit_code=$?
 run_test_suite "$tensorlake_sdk_test_files" "Tensorlake SDK"
-tests_exit_code=$((tests_exit_code || $?))
 
 if [ $tests_exit_code -eq 0 ]; then
-  echo "All tests passed!"
+  echo "All tests passed!" >> $summary_file
 else
-  echo "One or more tests failed. Please check output log for details."
+  echo "One or more tests failed. Please check output log for details." >> $summary_file
 fi
 
+cat $summary_file
 exit $tests_exit_code
