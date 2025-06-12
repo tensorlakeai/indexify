2ef957957d7a6e0de1fdd894482a83c668ea6004
Eugene Batalov <eugene@tensorlake.ai>
2025-06-12 15:41:01 +0100

    Update tensorlake, regen Py proto, bump version

--- indexify/tests/executor/test_graph_timeouts.py
+++ indexify/tests/executor/test_graph_timeouts.py
@@ -8,9 +8,7 @@ from tensorlake import (
     Graph,
     RemoteGraph,
     TensorlakeCompute,
-    TensorlakeRouter,
     tensorlake_function,
-    tensorlake_router,
 )
 from tensorlake.functions_sdk.graph_serialization import graph_code_dir_path
 from testing import test_graph_name
@@ -90,95 +88,6 @@ class TestFunctionTimeouts(unittest.TestCase):
         self.assertRegex(
             sdk_stdout.getvalue(),
             r"Function exceeded its configured timeout of.*3.000.*sec",
-        )
-
-
-class RouterThatSleepsForeverOnInitialization(TensorlakeRouter):
-    name = "RouterThatSleepsForeverOnInitialization"
-    timeout = 1
-
-    def __init__(self):
-        super().__init__()
-        time.sleep(1000000)
-
-    def run(self, action: str) -> List[
-        Union[
-            FunctionThatSleepsForeverOnInitialization,
-            function_that_sleeps_forever_when_running,
-        ]
-    ]:
-        raise Exception("This router can never run because it fails to initialize.")
-
-
-@tensorlake_router(timeout=2)
-def router_that_sleeps_forever_when_running() -> Union[
-    FunctionThatSleepsForeverOnInitialization,
-    function_that_sleeps_forever_when_running,
-]:
-    time.sleep(1000000)
-    return function_that_sleeps_forever_when_running
-
-
-class TestRouterTimeouts(unittest.TestCase):
-    def test_initilization(self):
-        graph = Graph(
-            name=test_graph_name(self),
-            description="test",
-            start_node=RouterThatSleepsForeverOnInitialization,
-        )
-        graph.route(
-            RouterThatSleepsForeverOnInitialization,
-            [
-                FunctionThatSleepsForeverOnInitialization,
-                function_that_sleeps_forever_when_running,
-            ],
-        )
-        graph = RemoteGraph.deploy(
-            graph=graph, code_dir_path=graph_code_dir_path(__file__)
-        )
-        start_time = time.monotonic()
-        invocation_id = graph.run(block_until_done=True)
-        duration_sec = time.monotonic() - start_time
-        self.assertLess(
-            duration_sec,
-            20,  # Add extra for state reporting and reconciliation latency
-            "Router initialization didn't timeout in duration close to 1 sec",
-        )
-
-    def test_run(self):
-        graph = Graph(
-            name=test_graph_name(self),
-            description="test",
-            start_node=router_that_sleeps_forever_when_running,
-        )
-        graph.route(
-            router_that_sleeps_forever_when_running,
-            [
-                FunctionThatSleepsForeverOnInitialization,
-                function_that_sleeps_forever_when_running,
-            ],
-        )
-        graph = RemoteGraph.deploy(
-            graph=graph, code_dir_path=graph_code_dir_path(__file__)
-        )
-        start_time = time.monotonic()
-        # We don't have a public SDK API to read a functions' stderr
-        # so we rely on internal SDK behavior where it prints a failed function's
-        # stderr to the current stdout.
-        sdk_stdout: io.StringIO = io.StringIO()
-        with redirect_stdout(sdk_stdout):
-            invocation_id = graph.run(block_until_done=True)
-        duration_sec = time.monotonic() - start_time
-        self.assertLess(
-            duration_sec,
-            20,  # Add extra for state reporting and reconciliation latency
-            "Router run didn't timeout in duration close to 2 sec",
-        )
-
-        # Use regex to ignore console formatting characters
-        self.assertRegex(
-            sdk_stdout.getvalue(),
-            r"Function exceeded its configured timeout of.*2.000.*sec",
         )
 
 
