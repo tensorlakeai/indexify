ab0fcec3fbe680324efe78a9da9e98c6c1fe9d7e
Eugene Batalov <eugene@tensorlake.ai>
2025-06-12 15:41:01 +0100

    Update tensorlake, regen Py proto, bump version

--- indexify/src/indexify/proto/executor_api_pb2.pyi
+++ indexify/src/indexify/proto/executor_api_pb2.pyi
@@ -1,6 +1,6 @@
+from collections.abc import Iterable as _Iterable
+from collections.abc import Mapping as _Mapping
 from typing import ClassVar as _ClassVar
-from typing import Iterable as _Iterable
-from typing import Mapping as _Mapping
 from typing import Optional as _Optional
 from typing import Union as _Union
 
