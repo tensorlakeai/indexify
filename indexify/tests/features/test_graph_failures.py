033d7c74413029fba5ffc4887c33e6841ec2cab3
Rob Earhart <rob@tensorlake.ai>
2025-06-14 13:24:31 -0700

    CR comments

--- indexify/tests/features/test_graph_failures.py
+++ indexify/tests/features/test_graph_failures.py
@@ -1,12 +1,11 @@
 import unittest
-from collections import defaultdict
 
-from rich import print
 from tensorlake import (
     Graph,
-    InvocationError,
+    InvocationArgumentError,
     RemoteGraph,
     Retries,
+    TensorlakeCompute,
     tensorlake_function,
 )
 from tensorlake.functions_sdk.graph_serialization import graph_code_dir_path
@@ -32,12 +31,23 @@ def to_remote_graph(graph) -> RemoteGraph:
 
 @tensorlake_function()
 def raise_permanent_invocation_error(x: str) -> int:
-    raise InvocationError("Testing permanent invocation errors")
+    raise InvocationArgumentError("Testing permanent invocation errors")
 
 
 @tensorlake_function()
 def raise_transient_invocation_error(x: str) -> int:
     raise Exception("Testing transient invocation errors")
+
+
+class FunctionThatThrowsFromInit(TensorlakeCompute):
+    name = "FunctionThatThrowsFromInit"
+
+    def __init__(self):
+        super().__init__()
+        raise Exception("Initialization Error")
+
+    def run(self, action: str) -> str:
+        raise Exception("This function can never run because it fails to initialize.")
 
 
 class TestGraphFailures(unittest.TestCase):
@@ -73,11 +83,44 @@ class TestGraphFailures(unittest.TestCase):
         im = graph.invocation_metadata(invocation_id)
         self.assertEqual("Failed", im.state.status)
 
-    def test_permanent_graph_failure(self):
+    def test_graph_remains_ready_after_transient_failures(self):
         graph = Graph(
             name=test_graph_name(self),
-            description="test for failed graph",
+            description="test for graph readiness after failed tasks",
             start_node=raise_transient_invocation_error,
+            retries=Retries(max_retries=3, max_delay=1.0),
+            consecutive_failure_max=3,
+        )
+
+        graph = to_remote_graph(graph)
+        invocation_id = graph.run(block_until_done=True, x=1)
+        gm = graph.metadata()
+        self.assertEqual(0, gm.failure_gauge.consecutive_failure_count)
+        self.assertEqual("Ready", gm.state.status)
+        im = graph.invocation_metadata(invocation_id)
+        self.assertEqual("Failed", im.state.status)
+
+        invocation_id = graph.run(block_until_done=True, x=1)
+        gm = graph.metadata()
+        self.assertEqual(0, gm.failure_gauge.consecutive_failure_count)
+        self.assertEqual("Ready", gm.state.status)
+        im = graph.invocation_metadata(invocation_id)
+        self.assertEqual("Failed", im.state.status)
+
+        invocation_id = graph.run(block_until_done=True, x=1)
+        output = graph.output(invocation_id, "raise_transient_invocation_error")
+        self.assertEqual(output, [])
+        gm = graph.metadata()
+        self.assertEqual(0, gm.failure_gauge.consecutive_failure_count)
+        self.assertEqual("Ready", gm.state.status)
+        im = graph.invocation_metadata(invocation_id)
+        self.assertEqual("Failed", im.state.status)
+
+    def test_graph_breaks_after_init_failures(self):
+        graph = Graph(
+            name=test_graph_name(self),
+            description="test for failed graph after init failures",
+            start_node=FunctionThatThrowsFromInit,
             retries=Retries(max_retries=3, max_delay=1.0),
             consecutive_failure_max=3,
         )
@@ -98,7 +141,7 @@ class TestGraphFailures(unittest.TestCase):
         self.assertEqual("Failed", im.state.status)
 
         invocation_id = graph.run(block_until_done=True, x=1)
-        output = graph.output(invocation_id, "raise_transient_invocation_error")
+        output = graph.output(invocation_id, "FunctionThatThrowsFromInit")
         self.assertEqual(output, [])
         gm = graph.metadata()
         self.assertEqual(3, gm.failure_gauge.consecutive_failure_count)
