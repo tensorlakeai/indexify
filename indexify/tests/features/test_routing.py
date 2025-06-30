18249d320f7b09afc30fd3be31edd07aa7cc37f8
Eugene Batalov <eugene@tensorlake.ai>
2025-06-30 14:35:08 +0100

    Add integration tests for Function and Function constructor retries

--- indexify/tests/features/test_routing.py
+++ indexify/tests/features/test_routing.py
@@ -4,26 +4,7 @@ from typing import List, Union
 from parameterized import parameterized_class
 from tensorlake import Graph, RemoteGraph, RouteTo, tensorlake_function
 from tensorlake.functions_sdk.graph_serialization import graph_code_dir_path
-
-
-def remote_or_local_graph(graph, remote=True) -> Union[RemoteGraph, Graph]:
-    if remote:
-        return RemoteGraph.deploy(graph, code_dir_path=graph_code_dir_path(__file__))
-    return graph
-
-
-def test_graph_name(test_case: unittest.TestCase) -> str:
-    """Converts a test case to a unique graph name.
-
-    Example:
-    >>> class TestGraphReduce(unittest.TestCase):
-    ...     def test_simple(self):
-    ...         g = Graph(name=graph_name(self), start_node=generate_seq)
-    ...         # ...
-    ...         print(g.name)
-    ...         # test_graph_reduce_test_simple
-    """
-    return unittest.TestCase.id(test_case).replace(".", "_")
+from testing import remote_or_local_graph, test_graph_name
 
 
 @tensorlake_function()
