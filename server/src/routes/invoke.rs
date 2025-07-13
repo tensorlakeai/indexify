e523267a9ac95d3cb4dc836065ced2699d9c81b7
Diptanu Gon Choudhury <diptanuc@gmail.com>
2025-07-12 22:59:29 -0700

    remove unused route

--- server/src/routes/invoke.rs
+++ server/src/routes/invoke.rs
@@ -3,21 +3,19 @@ use std::{collections::HashMap, time::Duration};
 use anyhow::anyhow;
 use axum::{
     body::Body,
-    extract::{Multipart, Path, Query, State},
+    extract::{Path, Query, State},
     http::HeaderMap,
     response::{sse::Event, IntoResponse},
-    Json,
 };
-use futures::{stream, Stream, StreamExt};
+use futures::{Stream, StreamExt};
 use tokio::sync::broadcast::{error::RecvError, Receiver};
 use tracing::{error, info, warn};
 use uuid::Uuid;
 
 use super::RouteState;
 use crate::{
-    blob_store::PutResult,
     data_model::{self, GraphInvocationCtxBuilder, InvocationPayloadBuilder},
-    http_objects::{GraphInputFile, IndexifyAPIError, InvocationId, InvocationQueryParams},
+    http_objects::{IndexifyAPIError, InvocationId, InvocationQueryParams},
     state_store::{
         invocation_events::{InvocationFinishedEvent, InvocationStateChangeEvent},
         requests::{InvokeComputeGraphRequest, RequestPayload, StateMachineUpdateRequest},
@@ -132,146 +130,6 @@ async fn create_invocation_event_stream(
             }
         }
     }
-}
-
-// #[allow(dead_code)]
-// #[derive(ToSchema)]
-// pub struct InvokeWithFile {
-//     /// Extra metadata for file
-//     metadata: Option<HashMap<String, serde_json::Value>>,
-//     #[schema(format = "binary")]
-//     /// Mime type of file
-//     mime_type: Option<String>,
-//     /// File to upload
-//     file: Option<String>,
-// }
-// /// Upload data to a compute graph
-// #[utoipa::path(
-//     post,
-//     path =
-// "/namespaces/{namespace}/compute_graphs/{compute_graph}/invoke_file",
-//     request_body(content_type = "multipart/form-data", content =
-// inline(InvokeWithFile)),     tag = "ingestion",
-//     responses(
-//         (status = 200, description = "upload successful"),
-//         (status = 400, description = "bad request"),
-//         (status = INTERNAL_SERVER_ERROR, description = "Internal Server
-// Error")     ),
-// )]
-pub async fn invoke_with_file(
-    Path((namespace, compute_graph)): Path<(String, String)>,
-    State(state): State<RouteState>,
-    Query(_params): Query<InvocationQueryParams>,
-    mut files: Multipart,
-) -> Result<Json<InvocationId>, IndexifyAPIError> {
-    let mut metadata: Option<serde_json::Value> = None;
-    let mut put_result: Option<PutResult> = None;
-
-    while let Some(field) = files.next_field().await.unwrap() {
-        if let Some(name) = field.name() {
-            if name == "file" {
-                let name = Uuid::new_v4().to_string();
-                info!(
-                    namespace = namespace,
-                    graph = compute_graph,
-                    "writing to blob store, file name = {:?}",
-                    name
-                );
-                let stream = field.map(|res| res.map_err(|err| anyhow::anyhow!(err)));
-                let res = state.blob_storage.put(&name, stream).await.map_err(|e| {
-                    error!(
-                        namespace = namespace,
-                        graph = compute_graph,
-                        "failed to write to blob store: {:?}",
-                        e
-                    );
-                    IndexifyAPIError::internal_error(anyhow!(
-                        "failed to write to blob store: {}",
-                        e
-                    ))
-                })?;
-                put_result = Some(res);
-            } else if name == "metadata" {
-                let text = field
-                    .text()
-                    .await
-                    .map_err(|e| IndexifyAPIError::bad_request(&e.to_string()))?;
-                let file_metadata = serde_json::from_str(&text)?;
-                metadata = Some(file_metadata);
-            }
-        }
-    }
-    if put_result.is_none() {
-        return Err(IndexifyAPIError::bad_request("file is required"));
-    }
-    let put_result = put_result.unwrap();
-    let payload = GraphInputFile {
-        metadata: metadata.unwrap_or_default(),
-        url: put_result.url.clone(),
-        sha_256: put_result.sha256_hash.clone(),
-        size: put_result.size_bytes,
-    };
-    let payload_key = Uuid::new_v4().to_string();
-    let payload_stream = stream::once(async move {
-        let payload_json = serde_json::to_string(&payload)?.as_bytes().to_vec().clone();
-        Ok(payload_json.into())
-    });
-    let put_result = state
-        .blob_storage
-        .put(&payload_key, Box::pin(payload_stream))
-        .await
-        .map_err(|e| {
-            error!("failed to write to blob store: {:?}", e);
-            IndexifyAPIError::internal_error(anyhow!("failed to upload content: {}", e))
-        })?;
-    let data_payload = data_model::DataPayload {
-        path: put_result.url,
-        size: put_result.size_bytes,
-        sha256_hash: put_result.sha256_hash,
-    };
-    let invocation_payload = InvocationPayloadBuilder::default()
-        .namespace(namespace.clone())
-        .compute_graph_name(compute_graph.clone())
-        .payload(data_payload)
-        .build()
-        .map_err(|e| {
-            IndexifyAPIError::internal_error(anyhow!("failed to upload content: {}", e))
-        })?;
-
-    let id = invocation_payload.id.clone();
-    let compute_graph = state
-        .indexify_state
-        .reader()
-        .get_compute_graph(&namespace, &compute_graph)
-        .map_err(|e| {
-            IndexifyAPIError::internal_error(anyhow!("failed to get compute graph: {}", e))
-        })?
-        .ok_or(IndexifyAPIError::not_found("compute graph not found"))?;
-    let graph_invocation_ctx = GraphInvocationCtxBuilder::default()
-        .namespace(namespace.to_string())
-        .compute_graph_name(compute_graph.name.to_string())
-        .graph_version(compute_graph.version.clone())
-        .invocation_id(invocation_payload.id.clone())
-        .fn_task_analytics(HashMap::new())
-        .created_at(invocation_payload.created_at)
-        .build(compute_graph.clone())
-        .map_err(|e| {
-            IndexifyAPIError::internal_error(anyhow!("failed to upload content: {}", e))
-        })?;
-    let request = RequestPayload::InvokeComputeGraph(InvokeComputeGraphRequest {
-        namespace: namespace.clone(),
-        compute_graph_name: compute_graph.name.clone(),
-        invocation_payload,
-        ctx: graph_invocation_ctx,
-    });
-    let sm_req = StateMachineUpdateRequest {
-        payload: request,
-        processed_state_changes: vec![],
-    };
-    state.indexify_state.write(sm_req).await.map_err(|e| {
-        IndexifyAPIError::internal_error(anyhow!("failed to upload content: {}", e))
-    })?;
-    Ok(Json(InvocationId { id }))
 }
 
 /// Invoke Compute Graph
